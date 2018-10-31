c31488227c0c35a79da25b98ab90ff290d9e7354
Julien Mounier <julien.mounier@tanker.io>
2018-10-31 13:56:54 +0100

    StreamDecryptor: add write and close method

--- packages/core/src/DataProtection/StreamDecryptor.js
+++ packages/core/src/DataProtection/StreamDecryptor.js
@@ -1,5 +1,10 @@
 // @flow
 
+import varint from 'varint';
+
+import { aead, tcrypto, type Key } from '@tanker/crypto';
+import { InvalidEncryptionFormat } from '../errors';
+import { type ResourceIdKeyPair } from '../Resource/ResourceManager';
 import { defaultBlockSize } from './StreamEncryptor';
 
 export type StreamDecryptorParameters = {
@@ -8,12 +13,39 @@ export type StreamDecryptorParameters = {
   blockSize?: number,
 }
 
+export type ResourceIdKeyMapper = {
+  findKey: (Uint8Array) => Promise<Key>
+}
+
 export default class StreamDecryptor {
+  _mapper: ResourceIdKeyMapper;
   _onData: (Uint8Array) => Promise<void> | void;
   _onEnd: () => Promise<void> | void;
   _blockSize: number = defaultBlockSize;
+  _resourceIdKeyPair: ?ResourceIdKeyPair;
+  _index = 0;
 
-  constructor(parameters: StreamDecryptorParameters) {
+  async _findAndRemoveKeyFromData(encryptedData: Uint8Array) {
+    const version = varint.decode(encryptedData);
+    const binaryData = encryptedData.subarray(varint.decode.bytes);
+
+    switch (version) {
+      case 1:
+      {
+        const resourceId = binaryData.subarray(0, tcrypto.MAC_SIZE);
+        return {
+          binaryData: binaryData.subarray(tcrypto.MAC_SIZE),
+          key: await this._mapper.findKey(resourceId),
+          resourceId
+        };
+      }
+      default:
+        throw new InvalidEncryptionFormat(`unhandled format version in StreamDecryptor: '${version}'`);
+    }
+  }
+
+  constructor(mapper: ResourceIdKeyMapper, parameters: StreamDecryptorParameters) {
+    this._mapper = mapper;
     this._onData = parameters.onData;
     this._onEnd = parameters.onEnd;
     if (parameters.blockSize) {
@@ -21,15 +53,27 @@ export default class StreamDecryptor {
     }
   }
 
-  write(clearData: Uint8Array): Promise<void> { // eslint-disable-line no-unused-vars
-    throw new Error('not implemented yet');
+  async write(encryptedData: Uint8Array): Promise<void> {
+    let data = encryptedData;
+    if (!this._resourceIdKeyPair) {
+      const { binaryData, key, resourceId } = await this._findAndRemoveKeyFromData(encryptedData);
+      this._resourceIdKeyPair = {
+        key,
+        resourceId
+      };
+      data = binaryData;
+    }
+
+    const subKey = tcrypto.deriveKey(this._resourceIdKeyPair.key, this._index);
+    this._index += 1;
+    return this._onData(await aead.decryptAEADv2(subKey, data));
   }
 
-  close(): Promise<void> {
-    throw new Error('not implemented yet');
+  async close(): Promise<void> {
+    return this._onEnd();
   }
 }
 
-export function makeStreamDecryptor(parameters: StreamDecryptorParameters): StreamDecryptor {
-  return new StreamDecryptor(parameters);
+export function makeStreamDecryptor(mapper: ResourceIdKeyMapper, parameters: StreamDecryptorParameters): StreamDecryptor {
+  return new StreamDecryptor(mapper, parameters);
 }
