aed612608cd880c29692aec0b8f8c37c0edf9815
Julien Mounier <julien.mounier@tanker.io>
2018-10-31 13:56:54 +0100

    StreamDecryptor: add write and close method

--- packages/core/src/DataProtection/DataProtector.js
+++ packages/core/src/DataProtection/DataProtector.js
@@ -187,6 +187,9 @@ export default class DataProtector {
   }
 
   async makeStreamDecryptor(parameters: StreamDecryptorParameters): Promise<StreamDecryptor> {
-    return makeStreamDecryptor(parameters);
+    const resourceIdKeyMapper = {
+      findKey: (resourceId) => this._resourceManager.findKeyFromResourceId(resourceId, true)
+    };
+    return makeStreamDecryptor(resourceIdKeyMapper, parameters);
   }
 }
