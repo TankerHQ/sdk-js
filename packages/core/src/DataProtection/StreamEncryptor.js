0068fecb8437bf74682d3917faa14c79455598c5
Julien Mounier <julien.mounier@tanker.io>
2018-11-05 13:38:48 +0100

    StreamEncryptor: add output buffering

--- packages/core/src/DataProtection/StreamEncryptor.js
+++ packages/core/src/DataProtection/StreamEncryptor.js
@@ -22,10 +22,12 @@ export default class StreamEncryptor {
   _onData: (Uint8Array) => Promise<void> | void;
   _onEnd: () => Promise<void> | void;
   _blockSize: number = defaultBlockSize;
+
   _resourceId: Uint8Array;
   _key: Uint8Array;
+
   _index = 0;
-  _formatHeader: ?Uint8Array;
+  _outputBuffer: Uint8Array = new Uint8Array(0);
 
   constructor(resourceId: Uint8Array, key: Uint8Array, parameters: StreamEncryptorParameters) {
     this._onData = parameters.onData;
@@ -37,24 +39,40 @@ export default class StreamEncryptor {
     this._key = key;
     this._resourceId = resourceId;
 
-    this._formatHeader = concatArrays(varint.encode(streamEncryptorVersion), resourceId);
+    this._outputBuffer = concatArrays(varint.encode(streamEncryptorVersion), resourceId);
   }
 
   resourceId(): b64string {
     return utils.toBase64(this._resourceId);
   }
 
-  async write(clearData: Uint8Array): Promise<void> {
-    if (this._formatHeader) {
-      await this._onData(this._formatHeader);
-      this._formatHeader = null;
+  async _handleBufferedOutput() {
+    let offset = 0;
+    let remaining = this._outputBuffer.length;
+
+    while (remaining >= this._blockSize) {
+      await this._onData(this._outputBuffer.subarray(offset, offset + this._blockSize));
+      offset += this._blockSize;
+      remaining -= this._blockSize;
     }
+
+    this._outputBuffer = this._outputBuffer.subarray(offset);
+  }
+
+  async write(clearData: Uint8Array): Promise<void> {
     const key = tcrypto.deriveKey(this._key, this._index);
     this._index += 1;
-    return this._onData(await aead.encryptAEADv2(key, clearData));
+    this._outputBuffer = concatArrays(this._outputBuffer, await aead.encryptAEADv2(key, clearData));
+
+    return this._handleBufferedOutput();
   }
 
   async close(): Promise<void> {
+    if (this._outputBuffer.length > this._blockSize) {
+      await this._handleBufferedOutput();
+    } else if (this._outputBuffer.length > 0) {
+      await this._onData(this._outputBuffer);
+    }
     return this._onEnd();
   }
 }
