6206f8fb5fac84339d5f6687bc5e47dc9e0b1ad3
Julien Mounier <julien.mounier@tanker.io>
2018-11-12 14:40:45 +0100

    feat(streams): add a ResizerStream
    This stream allows to output Uint8Array having a fixed size. It operates in
    object mode and bufferize the input until output size is reached.
    
    Buffering is backed by the Uint8Buffer structure introduced earlier.

--- packages/stream/base/src/index.js
+++ packages/stream/base/src/index.js
@@ -1,4 +1,5 @@
 // @flow
 export { Duplex, Readable, Transform, Writable } from 'readable-stream';
 
+export { default as ResizerStream } from './ResizerStream';
 export { default as Uint8Buffer } from './Uint8Buffer';
