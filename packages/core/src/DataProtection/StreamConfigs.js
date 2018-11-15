ce9a28e95fe39c355a275b76c534ee3e4380b880
Julien Mounier <julien.mounier@tanker.io>
2018-11-15 11:30:41 +0100

    feat(core): add InputStream + backpressure in StreamEncryption
    backpressure => resolving promise depends on write return value

--- packages/core/src/DataProtection/StreamConfigs.js
+++ packages/core/src/DataProtection/StreamConfigs.js
@@ -2,6 +2,7 @@
 
 import { type Key } from '@tanker/crypto';
 import { type ShareWithOptions } from './ShareWithOptions';
+import { Uint8Stream } from '../Uint8Stream';
 
 export const defaultEncryptionSize = Number('10e5');
 export const defaultOutputSize = defaultEncryptionSize;
@@ -27,4 +28,29 @@ export type StreamDecryptorParameters = {
 
 export type ResourceIdKeyMapper = {
   findKey: (Uint8Array) => Promise<Key>
+}
+
+export function configureInputStream(inputSize: number, callback: { onDrain: Function, onError: Function }) {
+  const inputStream = new Uint8Stream(inputSize);
+
+  inputStream.on('drain', callback.onDrain);
+  inputStream.on('error', callback.onError);
+  return inputStream;
+}
+
+export function configureOutputStream(outputSize: number, callback: { onData: Function, onEnd: Function, onError: Function }) {
+  const outputStream = new Uint8Stream(outputSize);
+
+  outputStream.on('data', async (data) => {
+    outputStream.pause();
+    try {
+      await callback.onData(data);
+    } catch (err) {
+      callback.onError(err);
+    }
+    outputStream.resume();
+  });
+  outputStream.on('end', callback.onEnd);
+  outputStream.on('error', callback.onError);
+  return outputStream;
 }
