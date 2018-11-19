27b1e535cfca885fb8855400aa87571fff83730e
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-11-19 13:32:07 +0100

    Move ci logic to a dedicated repository

---
+++ run-ci.py
@@ -0,0 +1,40 @@
+import argparse
+import sys
+
+from path import Path
+
+import ci.js
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title="subcommands", dest="command")
+
+    check_parser = subparsers.add_parser("check")
+    check_parser.add_argument("--nightly", action="store_true")
+    check_parser.add_argument("--runner", required=True)
+
+    deploy_parser = subparsers.add_parser("deploy")
+    deploy_parser.add_argument("--git-tag", required=True)
+    deploy_parser.add_argument("--env", required=True)
+
+    subparsers.add_parser("mirror")
+
+    args = parser.parse_args()
+    if args.command == "check":
+        runner = args.runner
+        nightly = args.nightly
+        ci.js.check_sdk(cwd=Path.getcwd(), env="dev", runner=runner, nightly=nightly)
+    elif args.command == "deploy":
+        env = args.env
+        git_tag = args.git_tag
+        ci.js.deploy_sdk(env=env, git_tag=git_tag)
+    elif args.command == "mirror":
+        ci.git.mirror(github_url="git@github.com:TankerHQ/sdk-js")
+    else:
+        parser.print_help()
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
