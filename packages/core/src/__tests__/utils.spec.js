d643680031b5c21d645d9cd03ed937453bedcfbc
jerem <jeremy.tellaa@tanker.io>
2018-11-28 18:46:06 +0100

    add unit tests on core utils

--- packages/core/src/__tests__/utils.spec.js
+++ packages/core/src/__tests__/utils.spec.js
@@ -2,7 +2,7 @@
 import { utils } from '@tanker/crypto';
 
 import { expect } from './chai';
-import { toBase64, fromBase64, toString, fromString, findIndex } from '../utils';
+import { toBase64, fromBase64, toString, fromString, findIndex, getTankerVersion, compareSameSizeUint8Arrays } from '../utils';
 import { errors } from '../index';
 
 const notStringTypes = [undefined, null, 0, {}, [], new Uint8Array(0)];
@@ -68,6 +68,33 @@ describe('utils', () => {
       expect(findIndex([0, 1, 2, 3, 4], (el) => el > 1)).to.equal(2);
       expect(findIndex(['a', 'b', 'c'], (el) => el === 'b')).to.equal(1);
       expect(findIndex([1, 3, 5, 7, 9], (el) => el % 2 === 0)).to.equal(-1);
+    });
+  });
+
+  describe('get tanker version', () => {
+    it('should look like a version', () => {
+      expect(typeof getTankerVersion()).to.equal('string');
+    });
+  });
+
+  describe('compare uint arrays', () => {
+    it('should throw when array does not have the same size', () => {
+      expect(() => compareSameSizeUint8Arrays(new Uint8Array(1), new Uint8Array(2))).to.throw();
+    });
+    it('should return 0 when arrays match', () => {
+      const left = new Uint8Array([45]);
+      const right = new Uint8Array([45]);
+      expect(compareSameSizeUint8Arrays(left, right)).to.equal(0);
+    });
+    it('should return 1 when left > right', () => {
+      const left = new Uint8Array([4, 46]);
+      const right = new Uint8Array([4, 45]);
+      expect(compareSameSizeUint8Arrays(left, right)).to.equal(1);
+    });
+    it('should return -1 when left < right', () => {
+      const left = new Uint8Array([4, 45]);
+      const right = new Uint8Array([4, 46]);
+      expect(compareSameSizeUint8Arrays(left, right)).to.equal(-1);
     });
   });
 });
