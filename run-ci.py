11638300c70187558b0cc07cc6e9e5dd45261830
jerem <jeremy.tellaa@tanker.io>
2018-11-30 15:27:02 +0100

    add a manual job for upgrade tests

--- run-ci.py
+++ run-ci.py
@@ -7,16 +7,17 @@ import ci.js
 import ci.mail
 
 
-def check(*, runner: str, nightly: bool) -> None:
+def check(*, runner: str, nightly: bool, upgrade: bool) -> None:
     env = "dev"
     ci.js.yarn_install_deps()
     if runner == "linux":
         ci.js.run_linters()
         ci.js.run_tests_in_node(env=env)
 
-    if nightly:
-        with ci.mail.notify_failure("sdk-js"):
-            ci.js.run_tests_in_browser_ten_times(env=env, runner=runner)
+    if nightly or upgrade:
+        if nightly:
+            with ci.mail.notify_failure("sdk-js"):
+                ci.js.run_tests_in_browser_ten_times(env=env, runner=runner)
         if runner == "linux":
             with ci.mail.notify_failure("upgrade tests"):
                 ci.js.run_sdk_upgrade_tests(env=env)
@@ -31,6 +32,7 @@ def main() -> None:
     check_parser = subparsers.add_parser("check")
     check_parser.add_argument("--nightly", action="store_true")
     check_parser.add_argument("--runner", required=True)
+    check_parser.add_argument("--upgrade", action="store_true")
 
     deploy_parser = subparsers.add_parser("deploy")
     deploy_parser.add_argument("--git-tag", required=True)
@@ -42,7 +44,8 @@ def main() -> None:
     if args.command == "check":
         runner = args.runner
         nightly = args.nightly
-        check(runner=runner, nightly=nightly)
+        upgrade = args.upgrade
+        check(runner=runner, nightly=nightly, upgrade=upgrade)
     elif args.command == "deploy":
         env = args.env
         git_tag = args.git_tag
