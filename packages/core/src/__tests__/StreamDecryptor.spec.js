385b4fe7689f629c3a5f9641147efd2c930a9f20
Julien Mounier <julien.mounier@tanker.io>
2018-12-07 16:16:36 +0100

    StreamEncryption: extends Transform

--- packages/core/src/__tests__/StreamDecryptor.spec.js
+++ packages/core/src/__tests__/StreamDecryptor.spec.js
@@ -7,8 +7,7 @@ import { utils, aead, tcrypto, type Key } from '@tanker/crypto';
 import { expect } from './chai';
 import StreamDecryptor from '../DataProtection/StreamDecryptor';
 import { concatArrays } from '../Blocks/Serialize';
-import { defaultOutputSize } from '../DataProtection/StreamConfigs';
-import { InvalidArgument, DecryptFailed, NotEnoughData, InvalidEncryptionFormat } from '../errors';
+import { InvalidArgument, NotEnoughData, InvalidEncryptionFormat, DecryptFailed } from '../errors';
 import PromiseWrapper from '../PromiseWrapper';
 
 async function encryptMsg(key, index, str) {
@@ -31,34 +30,23 @@ describe('Stream Decryptor', () => {
   let buffer: Array<Uint8Array>;
   let key;
   let mapper;
-  let streamConfig;
+
+  const watchStream = (stream) => {
+    const sync = new PromiseWrapper();
+    stream.on('data', (data) => buffer.push(data));
+    stream.on('error', (err) => sync.reject(err));
+    stream.on('end', () => sync.resolve());
+    return sync;
+  };
 
   before(() => {
-    buffer = [];
     key = utils.fromString('12345678123456781234567812345678');
     mapper = {
       findKey: () => Promise.resolve(key)
     };
-
-    streamConfig = {
-      onData: async (data) => {
-        buffer.push(data);
-        await Promise.resolve();
-      },
-      onEnd: async () => {
-        await Promise.resolve();
-      },
-      outputSize: defaultOutputSize
-    };
-
-    sinon.spy(streamConfig, 'onData');
-    sinon.spy(streamConfig, 'onEnd');
   });
 
-  afterEach(() => {
-    streamConfig.onData.resetHistory();
-    streamConfig.onEnd.resetHistory();
-    streamConfig.outputSize = defaultOutputSize;
+  beforeEach(() => {
     buffer = [];
   });
 
@@ -67,19 +55,20 @@ describe('Stream Decryptor', () => {
     const msg2 = await encryptMsg(key, 1, '2nd message');
     const encryptedMsgLength = msg1.encrypted.length;
 
-    const stream = new StreamDecryptor(mapper, streamConfig, encryptedMsgLength);
+    const stream = new StreamDecryptor(mapper, encryptedMsgLength);
+    const sync = watchStream(stream);
 
     setKey(stream, key);
 
-    await expect(stream.write(msg1.encrypted.subarray(0, 5))).to.be.fulfilled;
-    await expect(stream.write(msg1.encrypted.subarray(5))).to.be.fulfilled;
-    await expect(stream.write(msg2.encrypted)).to.be.fulfilled;
-    await stream.close();
+    stream.write(msg1.encrypted.subarray(0, 5));
+    stream.write(msg1.encrypted.subarray(5));
+    stream.write(msg2.encrypted);
+    stream.end();
 
-    expect(streamConfig.onEnd.calledOnce).to.be.true;
+    await expect(sync.promise).to.be.fulfilled;
 
-    const res1 = buffer[0].subarray(0, msg1.clear.length);
-    const res2 = buffer[0].subarray(msg1.clear.length);
+    const res1 = buffer[0];
+    const res2 = buffer[1];
     expect(res1).to.deep.equal(msg1.clear);
     expect(res2).to.deep.equal(msg2.clear);
   });
@@ -92,36 +81,18 @@ describe('Stream Decryptor', () => {
     const msg = await encryptMsg(key, 0, 'message');
 
     const encryptedMsgLength = msg.encrypted.length;
-    const stream = new StreamDecryptor(mapper, streamConfig, encryptedMsgLength);
+    const stream = new StreamDecryptor(mapper, encryptedMsgLength);
+    const sync = watchStream(stream);
 
-    await expect(stream.write(concatArrays(formatHeader, msg.encrypted))).to.be.fulfilled;
-    await stream.close();
+    stream.write(concatArrays(formatHeader, msg.encrypted));
+    stream.end();
+
+    await expect(sync.promise).to.be.fulfilled;
 
     expect(mapper.findKey.withArgs(resourceId).calledOnce).to.be.true;
     expect(buffer[0]).to.deep.equal(msg.clear);
 
     spy.restore();
-  });
-
-  it('forwards chunks of specified size to onData', async () => {
-    const msg = await encryptMsg(key, 0, 'message');
-    streamConfig.outputSize = 5;
-
-    let decryptedRessource = new Uint8Array(0);
-    const stream = new StreamDecryptor(mapper, streamConfig);
-    setKey(stream, key);
-
-    await expect(stream.write(msg.encrypted)).to.be.fulfilled;
-    await stream.close();
-
-    for (let i = 0; i < buffer.length - 1; i++) {
-      expect(buffer[i].length).to.be.equal(5);
-      decryptedRessource = utils.concatArrays(decryptedRessource, buffer[i]);
-    }
-
-    decryptedRessource = utils.concatArrays(decryptedRessource, buffer[buffer.length - 1]);
-
-    expect(decryptedRessource).to.deep.equal(msg.clear);
   });
 
   describe('Errors', () => {
@@ -140,76 +111,51 @@ describe('Stream Decryptor', () => {
     });
 
     it('throws InvalidArgument when writing anything else than Uint8Array', async () => {
-      const stream = new StreamDecryptor(mapper, streamConfig);
+      const stream = new StreamDecryptor(mapper);
+      const sync = watchStream(stream);
 
-      // $FlowExpectedError
-      await expect(stream.write(undefined)).to.be.rejectedWith(InvalidArgument);
-      // $FlowExpectedError
-      await expect(stream.write(10)).to.be.rejectedWith(InvalidArgument);
-      // $FlowExpectedError
-      await expect(stream.write(null)).to.be.rejectedWith(InvalidArgument);
-      // $FlowExpectedError
-      await expect(stream.write('fail')).to.be.rejectedWith(InvalidArgument);
-      // $FlowExpectedError
-      await expect(stream.write({})).to.be.rejectedWith(InvalidArgument);
+      stream.write('fail');
+
+      await expect(sync.promise).to.be.rejectedWith(InvalidArgument);
     });
 
     it('throws DecryptFailed when data is corrupted', async () => {
-      const sync = new PromiseWrapper();
-      let resultError;
-      const decryptor = new StreamDecryptor(mapper, {
-        onData: () => { },
-        onEnd: () => { },
-        onError: (err) => {
-          resultError = err;
-          sync.resolve();
-        }
-      }, ref[1].length);
+      const stream = new StreamDecryptor(mapper, ref[1].length);
+      const sync = watchStream(stream);
 
       ref[1][0] += 1;
-      await decryptor.write(ref[0]); // header
-      await decryptor.write(ref[1]); // corrupted chunk
-      await sync.promise;
-      expect(resultError).to.be.an.instanceof(DecryptFailed);
+      stream.write(ref[0]); // header
+      stream.write(ref[1]); // corrupted chunk
+
+      await expect(sync.promise).to.be.rejectedWith(DecryptFailed);
     });
 
     it('throws NotEnoughData when the header is not fully given during first write', async () => {
-      const decryptor = new StreamDecryptor(mapper, {
-        onData: () => { },
-        onEnd: () => { }
-      }, ref[1].length);
+      const stream = new StreamDecryptor(mapper, ref[1].length);
+      const sync = watchStream(stream);
 
       const incompleteHeader = ref[0].subarray(0, 1);
-      await expect(decryptor.write(incompleteHeader)).to.be.rejectedWith(NotEnoughData);
+      stream.write(incompleteHeader);
+      await expect(sync.promise).to.be.rejectedWith(NotEnoughData);
     });
 
     it('throws InvalidEncryptionFormat when the header is corrupted', async () => {
-      const decryptor = new StreamDecryptor(mapper, {
-        onData: () => { },
-        onEnd: () => { }
-      }, ref[1].length);
+      const stream = new StreamDecryptor(mapper, ref[1].length);
+      const sync = watchStream(stream);
       ref[0][0] += 1;
 
-      await expect(decryptor.write(ref[0])).to.be.rejectedWith(InvalidEncryptionFormat);
+      stream.write(ref[0]);
+      await expect(sync.promise).to.be.rejectedWith(InvalidEncryptionFormat);
     });
 
     it('throws DecryptFailed when data is written in wrong order', async () => {
-      const sync = new PromiseWrapper();
-      let resultError;
-      const decryptor = new StreamDecryptor(mapper, {
-        onData: () => { },
-        onEnd: () => { },
-        onError: (err) => {
-          resultError = err;
-          sync.resolve();
-        }
-      }, ref[1].length);
+      const stream = new StreamDecryptor(mapper, ref[1].length);
+      const sync = watchStream(stream);
 
-      await decryptor.write(ref[0]);
-      await decryptor.write(ref[2]);
+      stream.write(ref[0]);
+      stream.write(ref[2]);
 
-      await sync.promise;
-      expect(resultError).to.be.an.instanceof(DecryptFailed);
+      await expect(sync.promise).to.be.rejectedWith(DecryptFailed);
     });
   });
 });
