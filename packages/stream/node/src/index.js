afabc7664082ce08e91a6a7c766ab907ea77c844
maximerety <maxime.rety@gmail.com>
2018-12-20 10:33:29 +0100

    refactor(stream): get base Stream classes from @tanker/stream-base only
    Dependencies should look like:
    'readable-stream' <- '@tanker/stream-base' <- all '@tanker/*' packages that need streams

--- packages/stream/node/src/index.js
+++ packages/stream/node/src/index.js
@@ -1,6 +1,5 @@
 // @flow
-export { Readable as ReadableStream, Writable as WritableStream } from 'readable-stream';
-export { ResizerStream, Uint8Buffer } from '@tanker/stream-base';
+export { Duplex, Readable, Transform, Writable, ResizerStream, Uint8Buffer } from '@tanker/stream-base';
 
 export { default as MergerStream } from './MergerStream';
 export { default as SlicerStream } from './SlicerStream';
