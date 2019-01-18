9bb70687d771754b8d05c4ea5dd2a2b023f3671e
maximerety <maxime.rety@gmail.com>
2019-01-18 15:47:33 +0100

    refactor(client-*): make inheritance explicit to prepare addition of methods

--- packages/client-node/src/index.js
+++ packages/client-node/src/index.js
@@ -1,19 +1,19 @@
 // @flow
-import { Tanker as TankerCore } from '@tanker/core';
+import { Tanker as TankerCore, optionsWithDefaults, type TankerOptions } from '@tanker/core';
 import PouchDB from '@tanker/datastore-pouchdb-node';
 
-// Issue with Babel < 7: https://github.com/babel/babel/pull/6238
-//
-// When switching to Babel 7+, just replace the two exports below with:
-//
-//    export * from '@tanker/core'
-//
-export { errors, getTankerVersion, TankerStatus, createUserSecret, fromBase64, fromString, getResourceId, toBase64, toString } from '@tanker/core';
-export type { b64string } from '@tanker/core';
-
-export const Tanker = TankerCore.defaults({
+const defaultOptions = {
   dataStore: { adapter: PouchDB },
   sdkType: 'client-node'
-});
+};
 
+class Tanker extends TankerCore {
+  constructor(options: TankerOptions) {
+    super(optionsWithDefaults(options, defaultOptions));
+  }
+}
+
+export type { b64string } from '@tanker/core';
+export { errors, getTankerVersion, TankerStatus, createUserSecret, fromBase64, fromString, getResourceId, toBase64, toString } from '@tanker/core';
+export { Tanker };
 export default Tanker;
--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -3,7 +3,7 @@
 // import
 import { createUserSecretB64 as createUserSecret } from '@tanker/crypto';
 
-import { Tanker, TankerStatus, getResourceId } from './Tanker';
+import { Tanker, TankerStatus, getResourceId, optionsWithDefaults } from './Tanker';
 import * as errors from './errors';
 import { fromBase64, toBase64, fromString, toString, getTankerVersion } from './utils';
 import ChunkEncryptor from './DataProtection/ChunkEncryptor';
@@ -19,6 +19,7 @@ export type { TankerOptions } from './Tanker';
 export {
   errors,
   getTankerVersion,
+  optionsWithDefaults,
   Tanker,
   TankerStatus,
   createUserSecret,
