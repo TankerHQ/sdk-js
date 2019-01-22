2bbbecb70652d17a787baa7e2312cc1e5b74810d
maximerety <maxime.rety@gmail.com>
2019-01-22 15:16:24 +0100

    feat(errors): improve InvalidArgument message (see details)
    - don't print too big values (big Arrays...)
    - always print sizes of objects responding to .length

---
+++ packages/errors/src/__tests__/print.spec.js
@@ -0,0 +1,75 @@
+// @flow
+import { expect } from 'chai';
+
+import { safePrintType, safePrintValue } from '../print';
+
+describe('print', () => {
+  let values;
+  let expectedTypes;
+  let expectedValues;
+
+  before(() => {
+    values = [
+      undefined,
+      null,
+      'it is a piece of text',
+      32,
+      NaN,
+      function myFync() {},
+      () => 'anonymous',
+      {},
+      { a: { nested: 'key' } },
+      [],
+      [0, { a: 'a', b: 2 }, null],
+      new Uint8Array(5),
+      new Uint8Array(1000),
+      (new Uint8Array(5)).buffer,
+    ];
+
+    expectedTypes = [
+      'undefined',
+      'null',
+      'string',
+      'number',
+      'number',
+      'function',
+      'function',
+      'Object',
+      'Object',
+      'Array(0)',
+      'Array(3)',
+      'Uint8Array(5)',
+      'Uint8Array(1000)',
+      'ArrayBuffer',
+    ];
+
+    expectedValues = [
+      'undefined',
+      'null',
+      '"it is a piece of text"',
+      '32',
+      'NaN',
+      '[source code]',
+      '[source code]',
+      '{}',
+      '{"a":{"nested":"key"}}',
+      '[]',
+      '[0,{"a":"a","b":2},null]',
+      '{"0":0,"1":0,"2":0,"3":0,"4":0}',
+      '[too big to print]',
+      '{}',
+    ];
+  });
+
+  it('should print types correctly', () => {
+    for (let i = 0; i < values.length; i++) {
+      expect(safePrintType(values[i]), `failed type check #${i}`).to.equal(expectedTypes[i]);
+    }
+  });
+
+  it('should print values correctly', () => {
+    for (let i = 0; i < values.length; i++) {
+      expect(safePrintValue(values[i]), `failed type check #${i}`).to.equal(expectedValues[i]);
+    }
+  });
+});
---
+++ packages/errors/src/print.js
@@ -0,0 +1,59 @@
+// @flow
+
+// Use a RegExp since IE11 does not implement Function.prototype.name
+const constructorNameRegExp = /^.*(?:function|class) +([^( ]+).*$/;
+
+export const getConstructorName = (obj: Object) => {
+  const constructor = obj.constructor;
+
+  if (typeof constructor.name === 'string')
+    return constructor.name;
+
+  return constructor.toString().trim().split('\n')[0].replace(constructorNameRegExp, '$1');
+};
+
+export const safePrintType = (value: any) => {
+  try {
+    if (value === null)
+      return 'null';
+
+    if (typeof value === 'object') {
+      const constructorName = getConstructorName(value);
+      if ('length' in value)
+        return `${constructorName}(${value.length})`;
+      return constructorName;
+    }
+
+    return typeof value;
+  } catch (err) {
+    console.error(err);
+    return '[error printing type]';
+  }
+};
+
+// Note: not recursive for now
+export const safePrintValue = (value: any, maxLength: number = 100) => {
+  try {
+    if (value === undefined)
+      return 'undefined';
+    if (value === null)
+      return 'null';
+    if (typeof value === 'object') {
+      if ('length' in value && value.length > maxLength)
+        return '[too big to print]';
+    }
+    if (value !== value) // eslint-disable-line no-self-compare
+      return 'NaN';
+    if (typeof value === 'function')
+      return '[source code]';
+
+    try {
+      return JSON.stringify(value);
+    } catch (err) {
+      return `${value}`;
+    }
+  } catch (err) {
+    console.error(err);
+    return '[error printing value]';
+  }
+};
