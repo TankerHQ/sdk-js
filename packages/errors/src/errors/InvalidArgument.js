de5a07aa0669c6aadf911fb3a6a16465bdb76476
maximerety <maxime.rety@gmail.com>
2019-01-22 15:16:24 +0100

    feat(errors): improve InvalidArgument message (see details)
    - don't print too big values (big Arrays...)
    - always print sizes of objects responding to .length

--- packages/errors/src/errors/InvalidArgument.js
+++ packages/errors/src/errors/InvalidArgument.js
@@ -1,27 +1,9 @@
 // @flow
 import TankerError from '../TankerError';
-
-function getTypeAsString(value) {
-  // only check the built-ins we care about in the API
-  if (value instanceof Array)
-    return 'Array';
-  else if (value instanceof Uint8Array)
-    return 'Uint8Array';
-
-  return typeof value;
-}
+import { safePrintType, safePrintValue } from '../print';
 
 export default class InvalidArgument extends TankerError {
   constructor(name: string, expectedType: string, value: any) {
-    let quotedValue;
-    try {
-      quotedValue = JSON.stringify(value);
-    } catch (e) {
-      quotedValue = value;
-    }
-
-    const foundType = getTypeAsString(value);
-
-    super('invalid_argument', `name: ${name} (${expectedType}), value: ${quotedValue} (${foundType})`);
+    super('invalid_argument', `name: ${name} (${expectedType}), value: ${safePrintValue(value)} (${safePrintType(value)})`);
   }
 }
