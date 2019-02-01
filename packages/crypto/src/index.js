32d75fa31ca0def3d1ac5239bcbfd18c4399c07e
maximerety <maxime.rety@gmail.com>
2019-02-01 16:05:10 +0100

    feat(crypto): conversion helpers between integer and binary (see details)
    Helpers to convert a number from and to a Uint8Array representing
    the same number as a uint32le or uint64le.
    
    This work can be extended to support all uint{8, 16, 32, 64}{be, le} formats.

--- packages/crypto/src/index.js
+++ packages/crypto/src/index.js
@@ -5,6 +5,7 @@ import * as aead from './aead';
 import { random, createUserSecretBinary, createUserSecretB64, checkUserSecret, obfuscateUserId } from './random';
 import { generichash } from './hash';
 import * as utils from './utils';
+import * as number from './number';
 import type { b64string, safeb64string, Key } from './aliases';
 
 export {
@@ -16,6 +17,7 @@ export {
   checkUserSecret,
   obfuscateUserId,
   generichash,
+  number,
   utils,
 };
 
