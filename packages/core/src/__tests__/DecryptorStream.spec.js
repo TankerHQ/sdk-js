b6e1d591ba0f89844f292b047e4510c134e73dd4
maximerety <maxime.rety@gmail.com>
2019-02-04 16:31:02 +0100

    refactor(core): implement definitive v4 format in stream encryption

--- packages/core/src/__tests__/DecryptorStream.spec.js
+++ packages/core/src/__tests__/DecryptorStream.spec.js
@@ -1,90 +1,63 @@
 // @flow
-
 import sinon from 'sinon';
-import varint from 'varint';
 
-import { utils, aead, tcrypto, type Key } from '@tanker/crypto';
+import { utils, aead, random, tcrypto } from '@tanker/crypto';
 import { expect } from './chai';
 import DecryptorStream from '../DataProtection/DecryptorStream';
 import { concatArrays } from '../Blocks/Serialize';
 import { InvalidArgument, NotEnoughData, InvalidEncryptionFormat, DecryptFailed } from '../errors';
 import PromiseWrapper from '../PromiseWrapper';
-
-async function encryptMsg(key, index, str) {
-  const msg = utils.fromString(str);
-  return {
-    clear: msg,
-    encrypted: await aead.encryptAEADv2(tcrypto.deriveKey(key, index), msg)
-  };
-}
-
-function setKey(stream: DecryptorStream, key: Key) {
-  // eslint-disable-next-line no-underscore-dangle, no-param-reassign
-  stream._state.resourceIdKeyPair = {
-    key,
-    resourceId: new Uint8Array(tcrypto.MAC_SIZE)
-  };
-}
+import { currentStreamVersion, serializeHeaderV4 } from '../Resource/ResourceManager';
 
 describe('Decryptor Stream', () => {
   let buffer: Array<Uint8Array>;
   let key;
+  let resourceId;
   let mapper;
+  let stream;
+  let sync;
 
-  const watchStream = (stream) => {
-    const sync = new PromiseWrapper();
-    stream.on('data', (data) => buffer.push(data));
-    stream.on('error', (err) => sync.reject(err));
-    stream.on('end', () => sync.resolve());
-    return sync;
+  const watchStream = (str) => {
+    const pw = new PromiseWrapper();
+    buffer = [];
+    str.on('data', (data) => buffer.push(data));
+    str.on('error', (err) => pw.reject(err));
+    str.on('end', () => pw.resolve());
+    return pw;
+  };
+
+  const encryptMsg = (index, str) => {
+    const clear = utils.fromString(str);
+    const header = serializeHeaderV4({
+      version: currentStreamVersion,
+      resourceId,
+      encryptedChunkSize: clear.length + tcrypto.SYMMETRIC_ENCRYPTION_OVERHEAD + 21,
+    });
+    const ivSeed = random(tcrypto.XCHACHA_IV_SIZE);
+    const iv = tcrypto.deriveIV(ivSeed, index);
+    const encrypted = concatArrays(header, ivSeed, aead.encryptAEAD(key, iv, clear));
+    return { clear, encrypted };
   };
 
   before(() => {
-    key = utils.fromString('12345678123456781234567812345678');
+    key = random(tcrypto.SYMMETRIC_KEY_SIZE);
+    resourceId = random(16);
     mapper = {
       findKey: () => Promise.resolve(key)
     };
   });
 
   beforeEach(() => {
-    buffer = [];
+    stream = new DecryptorStream(mapper);
+    sync = watchStream(stream);
   });
 
-  it('derives its key and decrypt chunk of fixed size', async () => {
-    const msg1 = await encryptMsg(key, 0, '1st message');
-    const msg2 = await encryptMsg(key, 1, '2nd message');
-    const encryptedMsgLength = msg1.encrypted.length;
-
-    const stream = new DecryptorStream(mapper, encryptedMsgLength);
-    const sync = watchStream(stream);
-
-    setKey(stream, key);
-
-    stream.write(msg1.encrypted.subarray(0, 5));
-    stream.write(msg1.encrypted.subarray(5));
-    stream.write(msg2.encrypted);
-    stream.end();
-
-    await expect(sync.promise).to.be.fulfilled;
-
-    const res1 = buffer[0];
-    const res2 = buffer[1];
-    expect(res1).to.deep.equal(msg1.clear);
-    expect(res2).to.deep.equal(msg2.clear);
-  });
-
-  it('can extract resourceId from format header v4', async () => {
+  it('can extract header v4 and resource id', async () => {
     const spy = sinon.spy(mapper, 'findKey');
+    const msg = encryptMsg(0, '1st message');
+    const emptyMsg = encryptMsg(1, '');
 
-    const resourceId = utils.fromString('1234567812345678');
-    const formatHeader = concatArrays(varint.encode(4), resourceId);
-    const msg = await encryptMsg(key, 0, 'message');
-
-    const encryptedMsgLength = msg.encrypted.length;
-    const stream = new DecryptorStream(mapper, encryptedMsgLength);
-    const sync = watchStream(stream);
-
-    stream.write(concatArrays(formatHeader, msg.encrypted));
+    stream.write(concatArrays(msg.encrypted, emptyMsg.encrypted));
     stream.end();
 
     await expect(sync.promise).to.be.fulfilled;
@@ -95,66 +68,80 @@ describe('Decryptor Stream', () => {
     spy.restore();
   });
 
+  it('can decrypt chunks of fixed size', async () => {
+    const msg1 = encryptMsg(0, '1st message');
+    const msg2 = encryptMsg(1, '2nd message');
+    const emptyMsg = encryptMsg(2, '');
+
+    stream.write(msg1.encrypted.subarray(0, 21));
+    stream.write(msg1.encrypted.subarray(21));
+    stream.write(msg2.encrypted);
+    stream.write(emptyMsg.encrypted);
+    stream.end();
+
+    await expect(sync.promise).to.be.fulfilled;
+
+    expect(buffer.length).to.equal(2);
+    expect(buffer[0]).to.deep.equal(msg1.clear);
+    expect(buffer[1]).to.deep.equal(msg2.clear);
+  });
+
+  it('can decrypt chunks of fixed size except last one', async () => {
+    const msg1 = encryptMsg(0, '1st message');
+    const msg2 = encryptMsg(1, '2nd');
+
+    stream.write(msg1.encrypted.subarray(0, 21));
+    stream.write(msg1.encrypted.subarray(21));
+    stream.write(msg2.encrypted);
+    stream.end();
+
+    await expect(sync.promise).to.be.fulfilled;
+
+    expect(buffer.length).to.equal(2);
+    expect(buffer[0]).to.deep.equal(msg1.clear);
+    expect(buffer[1]).to.deep.equal(msg2.clear);
+  });
+
   describe('Errors', () => {
-    let ref;
+    let chunks;
 
     beforeEach(async () => {
-      ref = [];
-      const msg1 = await encryptMsg(key, 0, '1st message');
-      const msg2 = await encryptMsg(key, 1, '2nd message');
-      const resourceId = utils.fromString('1234567812345678');
-      const header = concatArrays(varint.encode(4), resourceId);
-
-      ref.push(header);
-      ref.push(msg1.encrypted);
-      ref.push(msg2.encrypted);
+      const msg1 = encryptMsg(0, '1st message');
+      const msg2 = encryptMsg(1, '2nd message');
+      chunks = [msg1.encrypted, msg2.encrypted];
     });
 
     it('throws InvalidArgument when writing anything else than Uint8Array', async () => {
-      const stream = new DecryptorStream(mapper);
-      const sync = watchStream(stream);
-
       stream.write('fail');
-
       await expect(sync.promise).to.be.rejectedWith(InvalidArgument);
     });
 
+    it('throws DecryptFailed when missing empty chunk after only maximum size chunks', async () => {
+      stream.write(chunks[0]); // valid chunk of the maximum size
+      stream.end();
+      await expect(sync.promise).to.be.rejectedWith(DecryptFailed);
+    });
+
     it('throws DecryptFailed when data is corrupted', async () => {
-      const stream = new DecryptorStream(mapper, ref[1].length);
-      const sync = watchStream(stream);
-
-      ref[1][0] += 1;
-      stream.write(ref[0]); // header
-      stream.write(ref[1]); // corrupted chunk
-
+      chunks[0][61] += 1;
+      stream.write(chunks[0]); // corrupted chunk
       await expect(sync.promise).to.be.rejectedWith(DecryptFailed);
     });
 
     it('throws NotEnoughData when the header is not fully given during first write', async () => {
-      const stream = new DecryptorStream(mapper, ref[1].length);
-      const sync = watchStream(stream);
-
-      const incompleteHeader = ref[0].subarray(0, 1);
+      const incompleteHeader = chunks[0].subarray(0, 1);
       stream.write(incompleteHeader);
       await expect(sync.promise).to.be.rejectedWith(NotEnoughData);
     });
 
     it('throws InvalidEncryptionFormat when the header is corrupted', async () => {
-      const stream = new DecryptorStream(mapper, ref[1].length);
-      const sync = watchStream(stream);
-      ref[0][0] += 1;
-
-      stream.write(ref[0]);
+      chunks[0][0] += 1;
+      stream.write(chunks[0]);
       await expect(sync.promise).to.be.rejectedWith(InvalidEncryptionFormat);
     });
 
     it('throws DecryptFailed when data is written in wrong order', async () => {
-      const stream = new DecryptorStream(mapper, ref[1].length);
-      const sync = watchStream(stream);
-
-      stream.write(ref[0]);
-      stream.write(ref[2]);
-
+      stream.write(chunks[1]);
       await expect(sync.promise).to.be.rejectedWith(DecryptFailed);
     });
   });
--- packages/core/src/__tests__/EncryptorStream.spec.js
+++ packages/core/src/__tests__/EncryptorStream.spec.js
@@ -1,16 +1,16 @@
 // @flow
-
 import { aead, random, tcrypto, utils } from '@tanker/crypto';
 
 import { expect } from './chai';
 import EncryptorStream from '../DataProtection/EncryptorStream';
 import { InvalidArgument } from '../errors';
 import PromiseWrapper from '../PromiseWrapper';
-import { getResourceId } from '../Resource/ResourceManager';
+import { currentStreamVersion, getResourceId, extractHeaderV4 } from '../Resource/ResourceManager';
 
 describe('Encryptor Stream', () => {
-  let buffer: Array<Uint8Array>;
+  const headerV4Length = 21;
 
+  let buffer: Array<Uint8Array>;
   let key;
   let resourceId;
 
@@ -20,6 +20,12 @@ describe('Encryptor Stream', () => {
     stream.on('error', (err) => sync.reject(err));
     stream.on('end', () => sync.resolve());
     return sync;
+  };
+
+  const splitArray = (a: Uint8Array, index: number): [Uint8Array, Uint8Array] => {
+    const head = a.subarray(0, index);
+    const tail = a.subarray(index);
+    return [head, tail];
   };
 
   before(() => {
@@ -51,7 +57,20 @@ describe('Encryptor Stream', () => {
     await sync.promise;
   });
 
-  it('outputs a resource from which you can get the resource id', async () => {
+  it('outputs a resource from which you can read the header', async () => {
+    const stream = new EncryptorStream(resourceId, key);
+    const sync = watchStream(stream);
+    stream.end();
+    await sync.promise;
+
+    const { header } = extractHeaderV4(buffer[0]);
+
+    expect(header.version).to.equal(currentStreamVersion);
+    expect(header.resourceId).to.deep.equal(resourceId);
+    expect(typeof header.encryptedChunkSize).to.equal('number');
+  });
+
+  it('outputs a resource from which you can directly get the resource id', async () => {
     const stream = new EncryptorStream(resourceId, key);
     const sync = watchStream(stream);
     stream.end();
@@ -60,7 +79,7 @@ describe('Encryptor Stream', () => {
     expect(getResourceId(buffer[0])).to.deep.equal(resourceId);
   });
 
-  it('derives its key and push header before encryption', async () => {
+  it('derives its iv and push header before encryption', async () => {
     const msg = utils.fromString('message');
     const stream = new EncryptorStream(resourceId, key);
     const sync = watchStream(stream);
@@ -73,22 +92,62 @@ describe('Encryptor Stream', () => {
 
     await expect(sync.promise).to.be.fulfilled;
 
-    expect(buffer.length).to.be.equal(2);
-    const header = buffer[0];
-    const eMsg = buffer[1];
+    expect(buffer.length).to.be.equal(1);
+    const { data, header } = extractHeaderV4(buffer[0]);
 
-    expect(header.subarray(1)).to.deep.equal(resourceId);
+    expect(header.resourceId).to.deep.equal(resourceId);
 
-    await expect(aead.decryptAEADv2(key, eMsg)).to.be.rejected;
-    expect(await aead.decryptAEADv2(tcrypto.deriveKey(key, 0), eMsg)).to.deep.equal(msg);
+    const eMsg = data.subarray(tcrypto.XCHACHA_IV_SIZE);
+    const ivSeed = data.subarray(0, tcrypto.XCHACHA_IV_SIZE);
+    const iv = tcrypto.deriveIV(ivSeed, 0);
+
+    expect(() => aead.decryptAEAD(key, ivSeed, eMsg)).to.throw;
+    expect(aead.decryptAEAD(key, iv, eMsg)).to.deep.equal(msg);
   });
 
   it('encrypts chunks of fixed size', async () => {
     const msg = utils.fromString('message');
 
-    const stream = new EncryptorStream(resourceId, key, msg.length);
+    const encryptedChunkSize = msg.length + tcrypto.SYMMETRIC_ENCRYPTION_OVERHEAD + headerV4Length;
+
+    const stream = new EncryptorStream(resourceId, key, encryptedChunkSize);
     const sync = watchStream(stream);
 
+    // push msg twice
+    stream.write(msg.subarray(0, 5));
+    stream.write(msg.subarray(5));
+    stream.write(msg);
+
+    stream.end();
+
+    await expect(sync.promise).to.be.fulfilled;
+
+    expect(buffer.length).to.equal(3);
+
+    buffer.forEach((chunk, index) => {
+      const { data, header } = extractHeaderV4(chunk);
+      expect(header.version).to.equal(currentStreamVersion);
+      expect(header.encryptedChunkSize).to.equal(encryptedChunkSize);
+      expect(header.resourceId).to.deep.equal(resourceId);
+
+      const [ivSeed, eMsg] = splitArray(data, tcrypto.XCHACHA_IV_SIZE);
+      const iv = tcrypto.deriveIV(ivSeed, index);
+
+      // Last chunk is an empty chunk added to check integrity (data has not been truncated)
+      const expectedMsg = index === 2 ? new Uint8Array(0) : msg;
+      expect(aead.decryptAEAD(key, iv, eMsg)).to.deep.equal(expectedMsg);
+    });
+  });
+
+  it('encrypts chunks of fixed size except last one', async () => {
+    const msg = utils.fromString('message');
+
+    const encryptedChunkSize = msg.length + tcrypto.SYMMETRIC_ENCRYPTION_OVERHEAD + headerV4Length;
+
+    const stream = new EncryptorStream(resourceId, key, encryptedChunkSize);
+    const sync = watchStream(stream);
+
+    // push msg twice + 1 more byte
     stream.write(msg.subarray(0, 5));
     stream.write(msg.subarray(5));
     stream.write(msg);
@@ -98,13 +157,19 @@ describe('Encryptor Stream', () => {
 
     await expect(sync.promise).to.be.fulfilled;
 
-    expect(buffer.length).to.be.equal(4);
-    const emsg1 = buffer[1];
-    const emsg2 = buffer[2];
-    const emsg3 = buffer[3];
+    expect(buffer.length).to.equal(3);
 
-    expect(await aead.decryptAEADv2(tcrypto.deriveKey(key, 0), emsg1)).to.deep.equal(msg);
-    expect(await aead.decryptAEADv2(tcrypto.deriveKey(key, 1), emsg2)).to.deep.equal(msg);
-    expect(await aead.decryptAEADv2(tcrypto.deriveKey(key, 2), emsg3)).to.deep.equal(msg.subarray(1));
+    buffer.forEach((chunk, index) => {
+      const { data, header } = extractHeaderV4(chunk);
+      expect(header.version).to.equal(currentStreamVersion);
+      expect(header.encryptedChunkSize).to.equal(encryptedChunkSize);
+      expect(header.resourceId).to.deep.equal(resourceId);
+
+      const [ivSeed, eMsg] = splitArray(data, tcrypto.XCHACHA_IV_SIZE);
+      const iv = tcrypto.deriveIV(ivSeed, index);
+
+      const expectedMsg = index === 2 ? msg.subarray(1) : msg;
+      expect(aead.decryptAEAD(key, iv, eMsg)).to.deep.equal(expectedMsg);
+    });
   });
 });
