18bbb4de120917644bc28078fefbe88820d0ea30
Alexandre Bossard <alexandre.bossard@tanker.io>
2019-02-07 11:19:17 +0100

    Remove inline source map for tests
    It screws up the reported error line when test fails.

--- config/tests/babel.setup.js
+++ config/tests/babel.setup.js
@@ -4,6 +4,5 @@ const getBabelConfig = require('../babel.config');
 module.exports = require('@babel/register')({
   // babelify everything except node_modules that are not our own stuff
   ignore: [file => file.includes('node_modules') && !file.includes('@tanker')],
-  sourceMaps: 'inline',
   ...getBabelConfig({ target: 'node', coverage: true }),
 });
