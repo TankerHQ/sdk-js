2c2f2e2059ebdf4bbc15afe389229cbb14ecb2cd
jerem <jeremy.tellaa@tanker.io>
2019-02-12 15:49:27 +0100

    add BrowserStack reporter

--- config/karma/karma.config.base.js
+++ config/karma/karma.config.base.js
@@ -37,7 +37,7 @@ module.exports = {
   // test results reporter to use
   // possible values: 'dots', 'progress'
   // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-  reporters: ['mocha'],
+  reporters: ['mocha', 'BrowserStack'],
 
   mochaReporter: {
     output: 'full',
