632a7d0204534f7c5903fb22c912b9a6d317d879
jerem <jeremy.tellaa@tanker.io>
2019-02-12 14:31:51 +0100

    put custom launchers in a separate file

--- config/karma/karma.config.base.js
+++ config/karma/karma.config.base.js
@@ -1,6 +1,7 @@
 // @noflow
 const path = require('path');
 const { makeBaseConfig } = require('../webpack.config.base');
+const { customLaunchers } = require('./launchers.js');
 
 module.exports = {
   // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -61,60 +62,7 @@ module.exports = {
   // how many browser should be started simultaneous
   concurrency: Infinity,
 
-  // Sandbox does not work in un-privileged dockers, so
-  // use a custom launcher when run in a docker container
-  customLaunchers: {
-    ChromiumInDocker: {
-      base: 'ChromiumHeadless',
-      flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-translate', '--disable-extensions'],
-    },
-    ChromeWindows: {
-      base: 'BrowserStack',
-      browser: 'Chrome',
-      os: 'Windows',
-      os_version: '10',
-    },
-    Chrome70Windows: {
-      base: 'BrowserStack',
-      browser: 'Chrome',
-      browser_version: '70.0',
-      os: 'Windows',
-      os_version: '10',
-    },
-    EdgeWindows: {
-      base: 'BrowserStack',
-      browser: 'Edge',
-      os: 'Windows',
-      os_version: '10',
-    },
-    IeWindows: {
-      base: 'BrowserStack',
-      browser: 'IE',
-      browser_version: '11.0',
-      os: 'Windows',
-      os_version: '7',
-    },
-    FirefoxWindows: {
-      base: 'BrowserStack',
-      browser: 'Firefox',
-      os: 'Windows',
-      os_version: '10',
-    },
-    Firefox64Windows: {
-      base: 'BrowserStack',
-      browser: 'Firefox',
-      browser_version: '64.0',
-      os: 'Windows',
-      os_version: '8.1',
-    },
-    SafariMojave: {
-      base: 'BrowserStack',
-      browser: 'Safari',
-      browser_version: '12.0',
-      os: 'OS X',
-      os_version: 'Mojave',
-    },
-  },
+  customLaunchers,
   browserStack: {
     project: 'sdk-js',
   },
---
+++ config/karma/launchers.js
@@ -0,0 +1,106 @@
+
+const customLaunchers = {
+
+  // Sandbox does not work in un-privileged dockers, so
+  // use a custom launcher when run in a docker container
+  ChromiumInDocker: {
+    base: 'ChromiumHeadless',
+    flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-translate', '--disable-extensions'],
+  },
+
+  // All sorts of BrowserStack browsers, you can create your using:
+  // https://www.browserstack.com/automate/capabilities
+  ChromeWindows: {
+    base: 'BrowserStack',
+    browser: 'Chrome',
+    os: 'Windows',
+    os_version: '10',
+  },
+  Chrome70Windows: {
+    base: 'BrowserStack',
+    browser: 'Chrome',
+    browser_version: '70.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+  ChromeWindows7: {
+    base: 'BrowserStack',
+    browser: 'Chrome',
+    os: 'Windows',
+    os_version: '7',
+  },
+  ChromeOsx: {
+    base: 'BrowserStack',
+    browser: 'Chrome',
+    os: 'OS X',
+    os_version: 'Mojave',
+  },
+  EdgeWindows: {
+    base: 'BrowserStack',
+    browser: 'Edge',
+    os: 'Windows',
+    os_version: '10',
+  },
+  IeWindows: {
+    base: 'BrowserStack',
+    browser: 'IE',
+    browser_version: '11.0',
+    os: 'Windows',
+    os_version: '7',
+  },
+  FirefoxWindows: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    os: 'Windows',
+    os_version: '10',
+  },
+  Firefox64Windows: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    browser_version: '64.0',
+    os: 'Windows',
+    os_version: '8.1',
+  },
+  FirefoxWindows7: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    os: 'Windows',
+    os_version: '7',
+  },
+  FirefoxOsx: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    os: 'OS X',
+    os_version: 'Mojave',
+  },
+  SafariMojave: {
+    base: 'BrowserStack',
+    browser: 'Safari',
+    browser_version: '12.0',
+    os: 'OS X',
+    os_version: 'Mojave',
+  },
+  SafariHighSierra: {
+    base: 'BrowserStack',
+    browser: 'Safari',
+    browser_version: '11.1',
+    os: 'OS X',
+    os_version: 'High Sierra',
+  },
+  SafariSierra: {
+    base: 'BrowserStack',
+    browser: 'Safari',
+    browser_version: '10.0',
+    os: 'OS X',
+    os_version: 'Sierra',
+  },
+  SafariElCapitan: {
+    base: 'BrowserStack',
+    browser: 'Safari',
+    browser_version: '9.1',
+    os: 'OS X',
+    os_version: 'El Capitan',
+  },
+}
+
+module.exports = { customLaunchers };
