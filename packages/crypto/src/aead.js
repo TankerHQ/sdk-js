96914c2f345ed98096d3b4c1f4f633bb53232e48
maximerety <maxime.rety@gmail.com>
2019-02-15 17:46:13 +0100

    cleanup(crypto): simplify aead crypto

--- packages/crypto/src/aead.js
+++ packages/crypto/src/aead.js
@@ -1,9 +1,6 @@
 // @flow
-
 import sodium from 'libsodium-wrappers';
-import * as tcrypto from './tcrypto';
-import { random } from './random';
-import { concatArrays } from './utils';
+import { MAC_SIZE } from './tcrypto';
 
 export function encryptAEAD(key: Uint8Array, iv: Uint8Array, plaintext: Uint8Array, associatedData?: Uint8Array): Uint8Array {
   return sodium.crypto_aead_xchacha20poly1305_ietf_encrypt(plaintext, associatedData, null, iv, key);
@@ -13,43 +10,9 @@ export function decryptAEAD(key: Uint8Array, iv: Uint8Array, ciphertext: Uint8Ar
   return sodium.crypto_aead_xchacha20poly1305_ietf_decrypt(null, ciphertext, associatedData, iv, key);
 }
 
-export async function encryptAEADv1(key: Uint8Array, plaintext: Uint8Array, associatedData?: Uint8Array): Promise<Uint8Array> {
-  const iv = random(tcrypto.XCHACHA_IV_SIZE);
-  const ciphertext = encryptAEAD(key, iv, plaintext, associatedData);
-  return concatArrays(ciphertext, iv);
-}
-
-export async function decryptAEADv1(key: Uint8Array, ciphertext: Uint8Array, associatedData?: Uint8Array): Promise<Uint8Array> {
-  const rawCiphertext = ciphertext.subarray(0, ciphertext.length - tcrypto.XCHACHA_IV_SIZE);
-  const iv = ciphertext.subarray(ciphertext.length - tcrypto.XCHACHA_IV_SIZE);
-  return decryptAEAD(key, iv, rawCiphertext, associatedData);
-}
-
-export async function encryptAEADv2(key: Uint8Array, plaintext: Uint8Array, associatedData?: Uint8Array): Promise<Uint8Array> {
-  const iv = random(tcrypto.XCHACHA_IV_SIZE);
-  const ciphertext = encryptAEAD(key, iv, plaintext, associatedData);
-  return concatArrays(iv, ciphertext);
-}
-
-export async function decryptAEADv2(key: Uint8Array, ciphertext: Uint8Array, associatedData?: Uint8Array): Promise<Uint8Array> {
-  const iv = ciphertext.subarray(0, tcrypto.XCHACHA_IV_SIZE);
-  const rawCiphertext = ciphertext.subarray(tcrypto.XCHACHA_IV_SIZE);
-  return decryptAEAD(key, iv, rawCiphertext, associatedData);
-}
-
-export async function encryptAEADv3(key: Uint8Array, plaintext: Uint8Array, associatedData?: Uint8Array): Promise<Uint8Array> {
-  const iv = new Uint8Array(tcrypto.XCHACHA_IV_SIZE); // zeros
-  return encryptAEAD(key, iv, plaintext, associatedData);
-}
-
-export async function decryptAEADv3(key: Uint8Array, ciphertext: Uint8Array, associatedData?: Uint8Array): Promise<Uint8Array> {
-  const iv = new Uint8Array(tcrypto.XCHACHA_IV_SIZE); // zeros
-  return decryptAEAD(key, iv, ciphertext, associatedData);
-}
-
 export function extractMac(edata: Uint8Array): Uint8Array {
-  if (edata.length < tcrypto.MAC_SIZE)
-    throw new Error(`Assertion error: at least ${tcrypto.MAC_SIZE} bytes needed to extract a MAC`);
+  if (edata.length < MAC_SIZE)
+    throw new Error(`Assertion error: at least ${MAC_SIZE} bytes needed to extract a MAC`);
 
-  return new Uint8Array(edata.subarray(edata.length - tcrypto.MAC_SIZE)); // don't use slice, doesn't work on IE11
+  return new Uint8Array(edata.subarray(edata.length - MAC_SIZE)); // don't use slice, doesn't work on IE11
 }
