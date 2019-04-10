30833bfe4611b6cdfdcd8badadb981d2b5fdc9b4
jerem <jeremy.tellaa@tanker.io>
2019-04-10 16:34:27 +0200

    compat: remove useless open/signIn/close/signOut
    This makes tests faster and easier to read

--- ci/compat/packages/tests/index.js
+++ ci/compat/packages/tests/index.js
@@ -11,44 +11,32 @@ import { makeCurrentUser, makeV1User } from './helpers';
 function generateEncryptTest(args: any) {
   it(`encrypts in ${args.version} and decrypts with current code`, async () => {
     const message = 'secret message';
-    await args.versionAlice.open();
     const encryptedData = await args.versionAlice.encrypt(message, [args.versionBob.id], []);
-    await args.versionAlice.close();
 
-    await args.currentBob.signIn();
     let decryptedData = await args.currentBob.decrypt(encryptedData);
     expect(decryptedData).to.equal(message);
-    await args.currentBob.signOut();
 
-    await args.currentAlice.signIn();
     decryptedData = await args.currentAlice.decrypt(encryptedData);
     expect(decryptedData).to.equal(message);
-    await args.currentAlice.signOut();
   });
 }
 
 function generateGroupTest(args: any) {
   it(`encrypts and shares with a group in ${args.version} and decrypts with current code`, async () => {
     let message = 'secret message for a group';
-    await args.versionAlice.open();
     const groupId = await args.versionAlice.createGroup([args.versionBob.id, args.versionAlice.id]);
     let encryptedData = await args.versionAlice.encrypt(message, [], [groupId]);
-    await args.versionAlice.close();
 
-    await args.currentBob.signIn();
     let decryptedData = await args.currentBob.decrypt(encryptedData);
     expect(decryptedData).to.equal(message);
 
-    await args.currentAlice.signIn();
     decryptedData = await args.currentAlice.decrypt(encryptedData);
     expect(decryptedData).to.equal(message);
     message = 'another secret message for a group';
     encryptedData = await args.currentAlice.encrypt(message, [], [groupId]);
-    await args.currentAlice.signOut();
 
     decryptedData = await args.currentBob.decrypt(encryptedData);
     expect(decryptedData).to.equal(message);
-    await args.currentBob.signOut();
   });
 }
 
@@ -86,12 +74,16 @@ function generateTests(opts: any) {
       args.currentAlice = makeCurrentUser(aliceId, aliceIdentity, args.trustchainId);
       args.versionAlice = makeV1User(opts.Tanker, aliceId, aliceToken, args.trustchainId);
       await args.versionBob.create();
-      await args.versionBob.close();
       await args.versionAlice.create();
-      await args.versionAlice.close();
+      await args.currentBob.signIn();
+      await args.currentAlice.signIn();
     });
 
     after(async () => {
+      await args.versionBob.close();
+      await args.versionAlice.close();
+      await args.currentBob.signOut();
+      await args.currentAlice.signOut();
       await args.trustchainHelper.cleanup();
     });
 
