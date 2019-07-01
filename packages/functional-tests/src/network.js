01c2457b06fc90ddde8e2cc8b64d2f0fc9e631e4
maximerety <maxime.rety@gmail.com>
2019-07-01 17:28:23 +0200

    test(core): rework network tests

---
+++ packages/functional-tests/src/network.js
@@ -0,0 +1,80 @@
+// @flow
+import { errors, type Tanker } from '@tanker/core';
+
+import { expect } from './chai';
+import { type TestArgs } from './TestArgs';
+
+const networkIssues = {
+  // closed_port: { url: 'https://api.tanker.io:666' }, // slow... (20 sec timeout)
+  // curl: (7) Failed to connect to api.tanker.io port 666: Operation timed out (75sec timeout)
+  dns_resolution_failed: { url: 'https://no-api.tanker.io' },
+  // curl: (6) Could not resolve host: noapi.tanker.io
+  non_routable_ip: { url: 'http://10.0.0.0' },
+  // curl: (7) Couldn't connect to server
+};
+
+const generateNetworkIssueTests = (args: TestArgs, issueType: string) => {
+  const clearText: string = 'Rivest Shamir Adleman';
+
+  describe(`with ${issueType.replace(/_/g, ' ')}`, () => {
+    let alicePhone;
+    let aliceIdentity;
+
+    const { url } = networkIssues[issueType];
+
+    const restartWithNetworkIssue = async (tankerInstance: Tanker, identity: string) => {
+      // "Kill" the server
+      await tankerInstance.stop();
+      tankerInstance._clientOptions.url = url; // eslint-disable-line
+      await tankerInstance.start(identity);
+    };
+
+    beforeEach(async () => {
+      aliceIdentity = await args.trustchainHelper.generateIdentity();
+      alicePhone = args.makeTanker();
+      await alicePhone.start(aliceIdentity);
+      await alicePhone.registerIdentity({ passphrase: 'passphrase' });
+    });
+
+    afterEach(async () => {
+      await alicePhone.stop();
+    });
+
+    it('can open a local session', async () => {
+      await restartWithNetworkIssue(alicePhone, aliceIdentity);
+    });
+
+    it('can decrypt a resource locally', async () => {
+      const encrypted = await alicePhone.encrypt(clearText);
+
+      await restartWithNetworkIssue(alicePhone, aliceIdentity);
+      const decrypted = await alicePhone.decrypt(encrypted);
+      expect(decrypted).to.equal(clearText);
+    });
+
+    it('throws if trying to encrypt', async () => {
+      await restartWithNetworkIssue(alicePhone, aliceIdentity);
+      await expect(alicePhone.encrypt(clearText)).to.be.rejectedWith(errors.NetworkError);
+    });
+
+    it('throws if trying to decrypt without the resource key', async () => {
+      await alicePhone.stop();
+
+      const aliceLaptop = args.makeTanker();
+      await aliceLaptop.start(aliceIdentity);
+      await aliceLaptop.verifyIdentity({ passphrase: 'passphrase' });
+      const encrypted = await aliceLaptop.encrypt(clearText);
+
+      await restartWithNetworkIssue(alicePhone, aliceIdentity);
+      await expect(alicePhone.decrypt(encrypted)).to.be.rejectedWith(errors.NetworkError);
+    });
+  });
+};
+
+const generateNetworkTests = (args: TestArgs) => {
+  describe('Network issues', () => {
+    Object.keys(networkIssues).forEach(type => generateNetworkIssueTests(args, type));
+  });
+};
+
+export default generateNetworkTests;
