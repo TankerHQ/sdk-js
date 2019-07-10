1808374bec1ffa1e1c24de2aedc27577ae0170f2
maximerety <maxime.rety@gmail.com>
2019-07-10 10:58:25 +0200

    feat(cloud-storage): implement chunked download using streams

---
+++ packages/client-browser/src/gcs/DownloadStream.js
@@ -0,0 +1,92 @@
+// @flow
+import { errors } from '@tanker/core';
+import { Readable } from '@tanker/stream-browser';
+
+import { simpleFetch } from '../http';
+
+export class DownloadStream extends Readable {
+  _chunkSize: number;
+  _downloadedLength: number;
+  _totalLength: number;
+  _url: string;
+  _verbose: bool;
+
+  constructor(url: string, chunkSize: number, verbose: bool = false) {
+    super({
+      objectMode: true
+    });
+
+    this._downloadedLength = 0;
+    this._chunkSize = chunkSize;
+    this._totalLength = 0;
+    this._url = url;
+    this._verbose = verbose;
+  }
+
+  log(message: string) {
+    if (this._verbose) {
+      console.log(`[DownloadStream] ${message}`); // eslint-disable-line no-console
+    }
+  }
+
+  async _read(/* size: number */) {
+    let result;
+
+    try {
+      const prevLength = this._downloadedLength;
+      const nextLength = prevLength + this._chunkSize;
+
+      const rangeHeader = `bytes=${prevLength}-${nextLength - 1}`;
+
+      this.log(`downloading chunk of size ${this._chunkSize} with header "Range: ${rangeHeader}"`);
+
+      const response = await simpleFetch(this._url, {
+        method: 'GET',
+        headers: { Range: rangeHeader },
+        responseType: 'arraybuffer',
+      });
+
+      const { ok, status, statusText, body, headers } = response;
+      // Note: status is usually 206 Partial Content
+      if (!ok) {
+        throw new errors.NetworkError(`GCS download request failed with status ${status}: ${statusText}`);
+      }
+
+      result = new Uint8Array(body);
+
+      if (!this._totalLength) {
+        // Partial content: file is incomplete (i.e. bigger than chunkSize)
+        if (status === 206) {
+          const header = headers['content-range']; // e.g. "bytes 786432-1048575/1048698"
+
+          if (typeof header !== 'string' || !header.match(/^bytes +\d+-\d+\/\d+$/)) {
+            throw new errors.NetworkError(`GCS answered with status 206 but an invalid content-range header: ${header}`);
+          }
+
+          this._totalLength = parseInt(header.split('/')[1], 0);
+        } else {
+          this._totalLength = result.length;
+        }
+
+        this.log(`found total length ${this._totalLength}`);
+      }
+
+      this.log(`received chunk of size ${result.length} and status ${status}`);
+      this._downloadedLength += result.length;
+    } catch (e) {
+      this.emit('error', e);
+      return;
+    }
+
+    this.push(result); // success
+
+    if (
+      (this._totalLength && this._downloadedLength === this._totalLength)
+      || (result.length < this._chunkSize)
+    ) {
+      this.push(null); // end
+    }
+  }
+}
+
+export default DownloadStream;
