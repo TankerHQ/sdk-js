9b5b5e376b4dbbf0b17f66e60dd655ec57f9d33f
Cedric GESTES <cedric.gestes@gmail.com>
2019-07-11 11:25:38 +0200

    feat: fake-auth: obfuscated user ids

--- packages/fake-authentication/src/FakeAuthentication.js
+++ packages/fake-authentication/src/FakeAuthentication.js
@@ -1,5 +1,6 @@
 //@flow
 import { encode, trim } from 'url-safe-base64';
+import { generichash, utils } from '@tanker/crypto';
 
 type PrivateIdentityResponse = {
   user_id: string,
@@ -12,6 +13,12 @@ type PublicIdentityResponse = {
   public_identity: string,
 };
 
+const { concatArrays, fromString, toSafeBase64 } = utils;
+
+export function obfuscateUserId(trustchainId: Uint8Array, userId: string): Uint8Array {
+  return toSafeBase64(generichash(concatArrays(fromString(userId), fromString(trustchainId))));
+}
+
 export default class FakeAuthentication {
   appId: string;
   url: string;
@@ -23,23 +30,30 @@ export default class FakeAuthentication {
 
   async getPrivateIdentity(userId: string) {
     const appId = this.appId;
+    const obsUserId = obfuscateUserId(appId, userId);
 
-    const response = await fetch(`${this.url}/apps/${trim(encode(appId))}/private_identity?user_id=${encodeURIComponent(userId)}`, {
+    const response = await fetch(`${this.url}/apps/${trim(encode(appId))}/private_identity?user_id=${encodeURIComponent(obsUserId)}`, {
       method: 'GET',
     });
     const json: PrivateIdentityResponse = await response.json();
-    return json;
+    return {
+      ...json,
+      user_id: userId,
+    };
   }
 
   async getUserPublicIdentities(userIds: Array<string>) {
     const appId = this.appId;
+    const obsUserIds = userIds.map(u => obfuscateUserId(appId, u));
 
-    const response = await fetch(`${this.url}/apps/${trim(encode(appId))}/public_identities?user_ids=${encodeURIComponent(userIds.join(','))}`, {
+    const response = await fetch(`${this.url}/apps/${trim(encode(appId))}/public_identities?user_ids=${encodeURIComponent(obsUserIds.join(','))}`, {
       method: 'GET',
     });
     const identitiesArray: Array<PublicIdentityResponse> = await response.json();
     const identities = identitiesArray.reduce((result, identity) => {
-      result[identity.user_id] = identity.public_identity; // eslint-disable-line no-param-reassign
+      const obsUserId = identity.user_id;
+      const idx = obsUserIds.indexOf(obsUserId);
+      result[userIds[idx]] = identity.public_identity; // eslint-disable-line no-param-reassign
       return result;
     }, {});
 
