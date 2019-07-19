fb8330f67a3d2ce9cd30f855655597a24e08fca5
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2019-07-19 15:14:16 +0200

    run-ci: import deploy_sdk() function from ci library

--- run-ci.py
+++ run-ci.py
@@ -43,6 +43,50 @@ def e2e(args) -> None:
     )
 
 
+def deploy_sdk(env: str, git_tag: str) -> None:
+    ci.js.yarn_install_deps()
+    version = ci.bump.version_from_git_tag(git_tag)
+    ci.bump.bump_files(version)
+
+    # Publish packages in order so that dependencies don't break during deploy
+    configs = [
+        {"build": "crypto", "publish": ["@tanker/crypto"]},
+        {"build": "errors", "publish": ["@tanker/errors"]},
+        {"build": "identity", "publish": ["@tanker/identity"]},
+        {"build": "file-ponyfill", "publish": ["@tanker/file-ponyfill"]},
+        {"build": "file-reader", "publish": ["@tanker/file-reader"]},
+        {
+            "build": "streams",
+            "publish": [
+                "@tanker/stream-base",
+                "@tanker/stream-browser",
+                "@tanker/stream-node",
+            ],
+        },
+        {
+            "build": "datastores",
+            "publish": [
+                "@tanker/datastore-base",
+                "@tanker/datastore-dexie-browser",
+                "@tanker/datastore-pouchdb-base",
+                "@tanker/datastore-pouchdb-memory",
+                "@tanker/datastore-pouchdb-node",
+            ],
+        },
+        {"build": "core", "publish": ["@tanker/core"]},
+        {"build": "client-browser", "publish": ["@tanker/client-browser"]},
+        {"build": "client-node", "publish": ["@tanker/client-node"]},
+        {"build": "fake-authentication", "publish": ["@tanker/fake-authentication"]},
+        {"build": "verification-ui", "publish": ["@tanker/verification-ui"]},
+        {"build": "filekit", "publish": ["@tanker/filekit"]},
+    ]
+
+    for config in configs:
+        ci.js.yarn_build(delivery=config["build"], env=env)  # type: ignore
+        for package_name in config["publish"]:
+            ci.js.publish_npm_package(package_name, version)
+
+
 def main() -> None:
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(title="subcommands", dest="command")
@@ -63,7 +107,6 @@ def main() -> None:
     e2e_parser.add_argument("--env", required=True)
     e2e_parser.add_argument("--use-local-sources", action="store_true", default=False)
 
-
     subparsers.add_parser("mirror")
 
     args = parser.parse_args()
@@ -73,7 +116,7 @@ def main() -> None:
         check(runner=runner, env=args.env, nightly=nightly)
     elif args.command == "deploy":
         git_tag = args.git_tag
-        ci.js.deploy_sdk(env=args.env, git_tag=git_tag)
+        deploy_sdk(env=args.env, git_tag=git_tag)
     elif args.command == "mirror":
         ci.git.mirror(github_url="git@github.com:TankerHQ/sdk-js")
     elif args.command == "compat":
