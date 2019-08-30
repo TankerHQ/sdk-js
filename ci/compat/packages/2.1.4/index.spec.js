5f70744f08555309cf9bb7c9637e48a399026b7b
jerem <jeremy.tellaa@tanker.io>
2019-08-30 15:24:51 +0200

    chore(tests): add compat tests for 2.1

---
+++ ci/compat/packages/2.1.4/index.spec.js
@@ -0,0 +1,13 @@
+// @flow
+import { Tanker } from '@tanker/client-node';
+import { createIdentity } from '@tanker/identity';
+import adapter from '@tanker/datastore-pouchdb-memory';
+
+import { generateV2Tests } from 'tests';
+
+generateV2Tests({
+  createIdentity,
+  Tanker,
+  tests: ['encrypt', 'group', 'verification', 'revocationV2'],
+  adapter,
+});
