9ed58fc3108c20fe286eb7083ba9279e236d9788
jerem <jeremy.tellaa@tanker.io>
2019-11-22 14:00:25 +0100

    chore(ci): use chrome instead of chromium

--- config/karma/launchers.js
+++ config/karma/launchers.js
@@ -7,8 +7,8 @@ const customLaunchers = {
 
   // Sandbox does not work in un-privileged dockers, so
   // use a custom launcher when run in a docker container
-  ChromiumInDocker: {
-    base: 'ChromiumHeadless',
+  ChromeInDocker: {
+    base: 'ChromeHeadless',
     flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-translate', '--disable-extensions'],
   },
 
