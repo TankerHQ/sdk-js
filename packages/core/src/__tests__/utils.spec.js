35cfd9bfaf84f3f427c4acd09e035729dc22c738
Aloïs <alois.jobard@tanker.io>
2019-12-05 17:14:10 +0100

    Chore(core): Remove unused code

--- packages/core/src/__tests__/utils.spec.js
+++ packages/core/src/__tests__/utils.spec.js
@@ -2,7 +2,7 @@
 import { InvalidArgument } from '@tanker/errors';
 import { expect } from '@tanker/test-utils';
 
-import { toBase64, fromBase64, toString, fromString, findIndex, compareSameSizeUint8Arrays } from '../utils';
+import { toBase64, fromBase64, findIndex } from '../utils';
 
 const notStringTypes = [undefined, null, 0, {}, [], new Uint8Array(0)];
 const notUint8ArrayTypes = [undefined, null, 0, {}, [], 'wat'];
@@ -20,16 +20,6 @@ describe('utils (core)', () => {
       // $FlowExpectedError
       notStringTypes.forEach((v, i) => expect(() => fromBase64(v), `#${i}`).to.throw(InvalidArgument));
     });
-
-    it('should throw when toString is given an invalid type', () => {
-      // $FlowExpectedError
-      notUint8ArrayTypes.forEach((v, i) => expect(() => toString(v), `#${i}`).to.throw(InvalidArgument));
-    });
-
-    it('should throw when fromString is given an invalid type', () => {
-      // $FlowExpectedError
-      notStringTypes.forEach((v, i) => expect(() => fromString(v), `#${i}`).to.throw(InvalidArgument));
-    });
   });
 
   describe('findIndex', () => {
@@ -37,30 +27,6 @@ describe('utils (core)', () => {
       expect(findIndex([0, 1, 2, 3, 4], (el) => el > 1)).to.equal(2);
       expect(findIndex(['a', 'b', 'c'], (el) => el === 'b')).to.equal(1);
       expect(findIndex([1, 3, 5, 7, 9], (el) => el % 2 === 0)).to.equal(-1);
-    });
-  });
-
-  describe('compare uint arrays', () => {
-    it('should throw when array does not have the same size', () => {
-      expect(() => compareSameSizeUint8Arrays(new Uint8Array(1), new Uint8Array(2))).to.throw();
-    });
-
-    it('should return 0 when arrays match', () => {
-      const left = new Uint8Array([45]);
-      const right = new Uint8Array([45]);
-      expect(compareSameSizeUint8Arrays(left, right)).to.equal(0);
-    });
-
-    it('should return 1 when left > right', () => {
-      const left = new Uint8Array([4, 46]);
-      const right = new Uint8Array([4, 45]);
-      expect(compareSameSizeUint8Arrays(left, right)).to.equal(1);
-    });
-
-    it('should return -1 when left < right', () => {
-      const left = new Uint8Array([4, 45]);
-      const right = new Uint8Array([4, 46]);
-      expect(compareSameSizeUint8Arrays(left, right)).to.equal(-1);
     });
   });
 });
