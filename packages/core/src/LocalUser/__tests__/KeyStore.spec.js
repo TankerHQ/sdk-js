741ac3a1c8f20c5d700a6c176bfc7ebb78426d9a
maximerety <maxime.rety@tanker.io>
2020-08-09 17:28:32 +0200

    refactor(core): new local device keys creation scheme
    The local device keys used to be created as session opening and persisted right away.
    
    Now, we create them in memory at user or additional device creation, and persist them
    locally, with the device id, only after the server response has been handled. Then, and
    only then, can the local device be used as a completely initialized device. If the
    process is interrupted before completion, the device keys will be overridden and a new
    device will be created.

--- packages/core/src/LocalUser/__tests__/KeyStore.spec.js
+++ packages/core/src/LocalUser/__tests__/KeyStore.spec.js
@@ -9,65 +9,48 @@ import dataStoreConfig, { makePrefix, openDataStore } from '../../__tests__/Test
 import KeyStore from '../KeyStore';
 
 describe('KeyStore', () => {
-  let dbName;
-  let keystoreConfig;
-  let datastore;
+  let datastoresToClose;
+  let openKeystore;
   let secret;
+
   const { schemas } = KeyStore;
 
-  const fakeDevice = {
-    deviceId: random(tcrypto.HASH_SIZE),
-    devicePublicEncryptionKey: random(tcrypto.ENCRYPTION_PUBLIC_KEY_SIZE),
-    devicePublicSignatureKey: random(tcrypto.SIGNATURE_PUBLIC_KEY_SIZE),
-    isGhostDevice: true,
-    revoked: true,
-  };
-
   beforeEach(async () => {
-    dbName = `keystore-test-${makePrefix()}`;
-    keystoreConfig = { ...dataStoreConfig, dbName, schemas };
-    datastore = await openDataStore(keystoreConfig);
+    datastoresToClose = [];
     secret = createUserSecretBinary('trustchainid', 'bob');
-  });
 
-  it('creates a safe when first opened that can be re-opened later', async () => {
-    const keystore1 = await KeyStore.open(datastore, secret);
-    const keys1 = keystore1.localData;
+    const dbName = `keystore-test-${makePrefix()}`;
 
-    await datastore.close();
-    datastore = await openDataStore(keystoreConfig);
-
-    const keystore2 = await KeyStore.open(datastore, secret);
-    const keys2 = keystore2.localData;
-
-    expect(keys1).to.deep.equal(keys2);
-  });
-
-  it('can set and save local data', async () => {
-    const keystore = await KeyStore.open(datastore, secret);
-    const localData = {
-      deviceSignatureKeyPair: keystore.localData.deviceSignatureKeyPair,
-      deviceEncryptionKeyPair: keystore.localData.deviceEncryptionKeyPair,
-      userKeys: { AAAAAAAA: tcrypto.makeEncryptionKeyPair() },
-      currentUserKey: tcrypto.makeEncryptionKeyPair(),
-      devices: [fakeDevice, fakeDevice],
-      deviceId: random(tcrypto.HASH_SIZE),
-      trustchainPublicKey: random(tcrypto.SIGNATURE_PUBLIC_KEY_SIZE),
+    openKeystore = async () => {
+      const datastore = await openDataStore({ ...dataStoreConfig, dbName, schemas });
+      datastoresToClose.push(datastore);
+      return KeyStore.open(datastore, secret);
     };
-    await keystore.save(localData, secret);
-
-    await datastore.close();
-    datastore = await openDataStore(keystoreConfig);
-
-    const keystore2 = await KeyStore.open(datastore, secret);
-    expect(keystore2.localData).to.deep.equal(localData);
   });
 
-  it('can cannot change device keys', async () => {
-    const keystore = await KeyStore.open(datastore, secret);
-    const deviceSignatureKeyPair = keystore.localData.deviceSignatureKeyPair;
-    const deviceEncryptionKeyPair = keystore.localData.deviceEncryptionKeyPair;
+  afterEach(async () => {
+    await Promise.all([datastoresToClose.map(d => d.close())]);
+  });
 
+  it('creates an "empty" safe when first opened', async () => {
+    const keystore = await openKeystore();
+    const localData = keystore.localData;
+
+    expect(localData.deviceId).to.be.null;
+    expect(localData.deviceEncryptionKeyPair).to.be.null;
+    expect(localData.deviceSignatureKeyPair).to.be.null;
+  });
+
+  it('can save local data, and retrieve data from the same keystore again', async () => {
+    const fakeDevice = {
+      deviceId: random(tcrypto.HASH_SIZE),
+      devicePublicEncryptionKey: random(tcrypto.ENCRYPTION_PUBLIC_KEY_SIZE),
+      devicePublicSignatureKey: random(tcrypto.SIGNATURE_PUBLIC_KEY_SIZE),
+      isGhostDevice: true,
+      revoked: true,
+    };
+
+    const keystore = await openKeystore();
     const localData = {
       deviceSignatureKeyPair: tcrypto.makeSignKeyPair(),
       deviceEncryptionKeyPair: tcrypto.makeEncryptionKeyPair(),
@@ -78,12 +61,9 @@ describe('KeyStore', () => {
       trustchainPublicKey: random(tcrypto.SIGNATURE_PUBLIC_KEY_SIZE),
     };
     await keystore.save(localData, secret);
+    expect(keystore.localData).to.deep.equal(localData);
 
-    await datastore.close();
-    datastore = await openDataStore(keystoreConfig);
-
-    const keystore2 = await KeyStore.open(datastore, secret);
-    expect(keystore2.localData.deviceEncryptionKeyPair).to.deep.equal(deviceEncryptionKeyPair);
-    expect(keystore2.localData.deviceSignatureKeyPair).to.deep.equal(deviceSignatureKeyPair);
+    const keystore2 = await openKeystore();
+    expect(keystore2.localData).to.deep.equal(localData);
   });
 });
