80ea733a49f7ad8d1d8e058cc5d3ef9d99b2d90c
maximerety <maxime.rety@tanker.io>
2020-12-04 11:23:37 +0100

    feat(http-utils): use native fetch if available instead of ponyfill
    The ponyfill is too limited for certain usages (e.g. referrerPolicy
    is not supported).

--- packages/http-utils/src/fetch.js
+++ packages/http-utils/src/fetch.js
@@ -1,8 +1,17 @@
 // @flow
 import { NetworkError } from '@tanker/errors';
+import globalThis from '@tanker/global-this';
 import fetchPonyfill from 'fetch-ponyfill';
 
-const { fetch: baseFetch } = fetchPonyfill({ Promise });
+// Use the window.fetch if available or the ponyfill otherwise (IE11, Node.js)
+// See: https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch#Browser_compatibility
+let baseFetch;
+
+if (globalThis.fetch && globalThis.Promise) {
+  baseFetch = globalThis.fetch;
+} else {
+  baseFetch = fetchPonyfill({ Promise }).fetch;
+}
 
 const fetch = (input: RequestInfo, init?: RequestOptions): Promise<Response> => baseFetch(input, init).catch(err => {
   throw new NetworkError(err.toString());
