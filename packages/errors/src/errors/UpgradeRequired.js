d33ae8e581f262c6f364759d2a0d837b0ac15279
maximerety <maxime.rety@tanker.io>
2021-01-19 16:59:15 +0100

    feat(errors): map server-sent UpgradeRequired errors
    The server can reject Core SDKs with versions that are not supported anymore.

--- packages/errors/src/errors/UpgradeRequired.js
+++ packages/errors/src/errors/UpgradeRequired.js
@@ -1,9 +1,13 @@
 // @flow
-import type { ErrorMessage } from '../ErrorInfo';
+import type { ErrorInfo } from '../ErrorInfo';
 import { TankerError } from '../TankerError';
 
 export class UpgradeRequired extends TankerError {
-  constructor(message: ErrorMessage) {
-    super('UpgradeRequired', `Tanker must be upgraded to a newer version to continue: ${message}`);
+  constructor(errorInfo?: ErrorInfo) {
+    if (typeof errorInfo === 'string') {
+      super('UpgradeRequired', `Tanker must be upgraded to a newer version to continue: ${errorInfo}`);
+    } else {
+      super('UpgradeRequired', errorInfo);
+    }
   }
 }
