3e8a700d8c5563a9cac44a22ad796185604dcb6b
julien mounier <julien.mounier@tanker.io>
2021-03-29 11:36:58 +0200

    test(upload): add createUpload/DownloadStream

---
+++ packages/functional-tests/src/helpers/stream.js
@@ -0,0 +1,10 @@
+// @flow
+import { Readable, Writable } from '@tanker/stream-base';
+
+// copy paste of the same function from core/src/CloudStorage/Manager.js
+export const pipeStreams = async (
+  { streams, resolveEvent }: { streams: Array<Readable | Writable>, resolveEvent: string }
+) => new Promise((resolve, reject) => {
+  streams.forEach(stream => stream.on('error', reject));
+  streams.reduce((leftStream, rightStream) => leftStream.pipe(rightStream)).on(resolveEvent, resolve);
+});
