cb924f17526b01446098d5cebcdbecc16080ce35
julien mounier <julien.mounier@tanker.io>
2021-03-30 15:29:49 +0200

    fix(stream): fix cloud-strorage DownloadStreams
    - use `destroy()` to report error cf:
      - https://nodejs.org/api/stream.html#stream_errors_while_reading
    - improve back pressure handling: use `push()` return value to know
      if the next request should be started

--- packages/stream/cloud-storage/src/gcs/DownloadStream.js
+++ packages/stream/cloud-storage/src/gcs/DownloadStream.js
@@ -11,6 +11,7 @@ export class DownloadStream extends Readable {
   _headUrl: string;
   _getUrl: string;
   _verbose: bool;
+  _readInProgress: bool;
 
   constructor(resourceId: string, headUrl: string, getUrl: string, chunkSize: number, verbose: bool = false) {
     super({
@@ -24,6 +25,7 @@ export class DownloadStream extends Readable {
     this._headUrl = headUrl;
     this._getUrl = getUrl;
     this._verbose = verbose;
+    this._readInProgress = false;
   }
 
   log(message: string) {
@@ -49,70 +51,82 @@ export class DownloadStream extends Readable {
   }
 
   async _read(/* size: number */) {
-    let result;
+    if (this._readInProgress)
+      return;
+
+    this._readInProgress = true;
 
     try {
-      const prevLength = this._downloadedLength;
-      const nextLength = prevLength + this._chunkSize;
+      while (await this._readRequest());
+    } catch (e) {
+      this.destroy(e);
+      return;
+    } finally {
+      this._readInProgress = false;
+    }
+  }
 
-      const rangeHeader = `bytes=${prevLength}-${nextLength - 1}`;
+  async _readRequest() {
+    const prevLength = this._downloadedLength;
+    const nextLength = prevLength + this._chunkSize;
 
-      const response = await retry(async () => {
-        this.log(`downloading chunk of size ${this._chunkSize} with header "Range: ${rangeHeader}"`);
+    const rangeHeader = `bytes=${prevLength}-${nextLength - 1}`;
 
-        const resp = await fetch(this._getUrl, {
-          method: 'GET',
-          headers: { Range: rangeHeader },
-        });
+    const response = await retry(async () => {
+      this.log(`downloading chunk of size ${this._chunkSize} with header "Range: ${rangeHeader}"`);
 
-        const { ok, status, statusText } = resp;
-        // Note: status is usually 206 Partial Content
-        if (!ok) {
-          throw new NetworkError(`GCS download request failed with status ${status}: ${statusText}`);
-        }
-
-        return resp;
-      }, {
-        retries: 2,
+      const resp = await fetch(this._getUrl, {
+        method: 'GET',
+        headers: { Range: rangeHeader },
       });
 
-      const { status, headers } = response;
-      const body = await response.arrayBuffer();
-
-      result = new Uint8Array(body);
-
-      if (!this._totalLength) {
-        // Partial content: file is incomplete (i.e. bigger than chunkSize)
-        if (status === 206) {
-          const header = headers.get('content-range') || ''; // e.g. "bytes 786432-1048575/1048698"
-
-          if (typeof header !== 'string' || !header.match(/^bytes +\d+-\d+\/\d+$/)) {
-            throw new NetworkError(`GCS answered with status 206 but an invalid content-range header: ${header}`);
-          }
-
-          this._totalLength = parseInt(header.split('/')[1], 10);
-        } else {
-          this._totalLength = result.length;
-        }
-
-        this.log(`found total length ${this._totalLength}`);
+      const { ok, status, statusText } = resp;
+      // Note: status is usually 206 Partial Content
+      if (!ok) {
+        throw new NetworkError(`GCS download request failed with status ${status}: ${statusText}`);
       }
 
-      this.log(`received chunk of size ${result.length} and status ${status}`);
-      this._downloadedLength += result.length;
-    } catch (e) {
-      this.emit('error', e);
-      return;
+      return resp;
+    }, {
+      retries: 2,
+    });
+
+    const { status, headers } = response;
+    const body = await response.arrayBuffer();
+
+    const result = new Uint8Array(body);
+
+    if (!this._totalLength) {
+      // Partial content: file is incomplete (i.e. bigger than chunkSize)
+      if (status === 206) {
+        const header = headers.get('content-range') || ''; // e.g. "bytes 786432-1048575/1048698"
+
+        if (typeof header !== 'string' || !header.match(/^bytes +\d+-\d+\/\d+$/)) {
+          throw new NetworkError(`GCS answered with status 206 but an invalid content-range header: ${header}`);
+        }
+
+        this._totalLength = parseInt(header.split('/')[1], 10);
+      } else {
+        this._totalLength = result.length;
+      }
+
+      this.log(`found total length ${this._totalLength}`);
     }
 
-    this.push(result); // success
+    this.log(`received chunk of size ${result.length} and status ${status}`);
+    this._downloadedLength += result.length;
+
+    const pushMore = this.push(result); // success
 
     if (
       (this._totalLength && this._downloadedLength === this._totalLength)
       || (result.length < this._chunkSize)
     ) {
       this.push(null); // end
+      return false;
     }
+
+    return pushMore;
   }
 }
 
