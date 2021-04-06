91d34162391d810b985d471a87a37958b03a5fe0
maximerety <maxime.rety@tanker.io>
2021-04-06 22:14:16 +0200

    test(stream): add more tests and constraining expectations

--- packages/stream/base/src/__tests__/ResizerStream.spec.js
+++ packages/stream/base/src/__tests__/ResizerStream.spec.js
@@ -15,20 +15,30 @@ describe('ResizerStream', () => {
     buffer = [];
   });
 
-  [1, 10].forEach((size) => {
-    it(`can split in chunks of size: ${size}`, async () => {
-      const stream = new ResizerStream(size);
-      stream.on('data', callback);
+  [5, 30].forEach((dataSize) => {
+    // Create a buffer with consecutive integers: 0, 1, 2, ...
+    const data = new Uint8Array(Array.from({ length: dataSize }, (_, k) => k % 256));
 
-      const data = new Uint8Array(30);
+    [1, 4, 7, 10].forEach((outputSize) => {
+      it(`can split a buffer of size ${dataSize} in chunks of size ${outputSize}`, async () => {
+        const expectedChunkCount = Math.ceil(dataSize / outputSize);
+        const expectedChunks = new Array(expectedChunkCount);
+        for (let i = 0; i < expectedChunkCount; i++) {
+          expectedChunks[i] = data.subarray(i * outputSize, Math.min((i + 1) * outputSize, dataSize));
+        }
 
-      stream.write(data);
-      let i = 0;
-      for (; i < buffer.length - 1; ++i) {
-        expect(buffer[i].length).to.equal(size);
-      }
-      const totalSize = i * size + buffer[buffer.length - 1].length;
-      expect(totalSize).to.be.equal(data.length);
+        const stream = new ResizerStream(outputSize);
+
+        await new Promise((resolve, reject) => {
+          stream.on('data', callback);
+          stream.on('error', reject);
+          stream.on('end', resolve);
+          stream.write(data);
+          stream.end();
+        });
+
+        expect(buffer).to.deep.equal(expectedChunks);
+      });
     });
   });
 
@@ -39,17 +49,19 @@ describe('ResizerStream', () => {
     const data1 = new Uint8Array(10);
     const data2 = new Uint8Array(10);
 
-    stream.write(data1);
-    stream.write(data2);
+    await new Promise((resolve, reject) => {
+      stream.on('error', reject);
+      stream.on('end', resolve);
 
-    expect(buffer.length).to.be.equal(0);
+      stream.write(data1);
+      stream.write(data2);
+      expect(buffer.length).to.be.equal(0);
 
-    stream.on('end', () => {
-      expect(buffer.length).to.be.equal(1);
-      expect(buffer[0].length).to.be.equal(20);
+      stream.end();
     });
 
-    stream.end();
+    expect(buffer.length).to.be.equal(1);
+    expect(buffer[0].length).to.be.equal(20);
   });
 
   it('stores data until outputSize is reached', async () => {
@@ -67,22 +79,5 @@ describe('ResizerStream', () => {
 
     expect(buffer.length).to.be.equal(1);
     expect(buffer[0].length).to.be.equal(20);
-  });
-
-  it('returns data in written order', () => {
-    const stream = new ResizerStream(20);
-    stream.on('data', callback);
-
-    const data = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
-    for (let i = 0; i < data.length; ++i) {
-      stream.write(data.subarray(i, i + 1));
-    }
-
-    stream.on('end', () => {
-      expect(buffer[0]).to.deep.equal(data);
-    });
-
-    stream.end();
   });
 });
