1d29c86acc9a68f44602f7474428f22117a4c590
maximerety <maxime.rety@tanker.io>
2021-06-04 17:39:37 +0200

    feat(core): improve resource key lookup performance
    Avoid fetching the same single resource key concurrently, either from
    Tanker servers, or from the local storage.
    
    This can improve the performance of parallel decryption operations
    drastically (e.g. when decrypting data encrypted with an encryption
    session).

--- packages/core/src/Resources/Manager.js
+++ packages/core/src/Resources/Manager.js
@@ -1,5 +1,5 @@
 // @flow
-import { type Key } from '@tanker/crypto';
+import { utils, type Key, type b64string } from '@tanker/crypto';
 
 import { getKeyPublishEntryFromBlock } from './Serialize';
 import { KeyDecryptor } from './KeyDecryptor';
@@ -11,9 +11,10 @@ import GroupManager from '../Groups/Manager';
 import ProvisionalIdentityManager from '../ProvisionalIdentity/Manager';
 
 export class ResourceManager {
-  _resourceStore: ResourceStore;
-  _client: Client;
-  _keyDecryptor: KeyDecryptor;
+  declare _client: Client;
+  declare _keyDecryptor: KeyDecryptor;
+  declare _keyLookupsInProgress: { [b64ResourceId: b64string]: Promise<Key>};
+  declare _resourceStore: ResourceStore;
 
   constructor(
     client: Client,
@@ -22,19 +23,34 @@ export class ResourceManager {
     groupManager: GroupManager,
     provisionalIdentityManager: ProvisionalIdentityManager
   ) {
-    this._resourceStore = resourceStore;
     this._client = client;
     this._keyDecryptor = new KeyDecryptor(localUserManager, groupManager, provisionalIdentityManager);
+    this._keyLookupsInProgress = {};
+    this._resourceStore = resourceStore;
   }
 
   async findKeyFromResourceId(resourceId: Uint8Array): Promise<Key> {
+    const b64resourceId = utils.toBase64(resourceId);
+
+    if (!this._keyLookupsInProgress[b64resourceId]) {
+      this._keyLookupsInProgress[b64resourceId] = this._findKeyFromResourceId(resourceId).finally(() => {
+        delete this._keyLookupsInProgress[b64resourceId];
+      });
+    }
+
+    return this._keyLookupsInProgress[b64resourceId];
+  }
+
+  async _findKeyFromResourceId(resourceId: Uint8Array): Promise<Key> {
     let resourceKey = await this._resourceStore.findResourceKey(resourceId);
+
     if (!resourceKey) {
       const keyPublishBlock = await this._client.getResourceKey(resourceId);
       const keyPublish = getKeyPublishEntryFromBlock(keyPublishBlock);
       resourceKey = await this._keyDecryptor.keyFromKeyPublish(keyPublish);
       await this._resourceStore.saveResourceKey(resourceId, resourceKey);
     }
+
     return resourceKey;
   }
 
