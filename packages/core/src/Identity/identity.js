a9089fc5e3bd0f9f0120ef56e1161c9737295b5d
Timothée Isnard <timothee.isnard@tanker.io>
2021-06-07 10:25:57 +0200

    feat: Handle new 'hashed_email' public provisional identities

--- packages/core/src/Identity/identity.js
+++ packages/core/src/Identity/identity.js
@@ -3,7 +3,8 @@ import { tcrypto, utils, type b64string } from '@tanker/crypto';
 import { InvalidArgument } from '@tanker/errors';
 
 type PermanentIdentityTarget = 'user';
-type ProvisionalIdentityTarget = 'email';
+type SecretProvisionalIdentityTarget = 'email';
+type PublicProvisionalIdentityTarget = 'email' | 'hashed_email';
 
 export type PublicPermanentIdentity = {|
   trustchain_id: b64string,
@@ -21,7 +22,7 @@ export type SecretPermanentIdentity = {|
 
 export type PublicProvisionalIdentity = {|
   trustchain_id: b64string,
-  target: ProvisionalIdentityTarget,
+  target: PublicProvisionalIdentityTarget,
   value: string,
   public_signature_key: b64string,
   public_encryption_key: b64string,
@@ -29,6 +30,7 @@ export type PublicProvisionalIdentity = {|
 
 export type SecretProvisionalIdentity = {|
   ...PublicProvisionalIdentity,
+  target: SecretProvisionalIdentityTarget,
   private_encryption_key: b64string,
   private_signature_key: b64string,
 |};
@@ -62,7 +64,7 @@ function isPublicPermanentIdentity(identity: SecretPermanentIdentity | PublicPer
 }
 
 function isProvisionalIdentity(identity: SecretIdentity | PublicIdentity): bool %checks {
-  return identity.target === 'email';
+  return !isPermanentIdentity(identity);
 }
 
 function _deserializeAndFreeze(identity: b64string): Object { // eslint-disable-line no-underscore-dangle
