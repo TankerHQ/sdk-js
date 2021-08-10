e24a9a3439c36a711d21599dcda321b9deb8c109
julien mounier <julien.mounier@tanker.io>
2021-08-10 10:06:55 +0200

    fix(compat): do not use aliases in module resolution
    using aliases prevents older sdk from using the package in node_module

---
+++ config/babel.require-no-alias-hook.js
@@ -0,0 +1,17 @@
+// @noflow
+const getBabelConfig = require('./babel.config');
+
+const coverage = !['false', '0'].includes(process.env.COVERAGE);
+
+// Install the "require hook" which will:
+//   - bind itself to node's require
+//   - automatically compile on the fly all the files subsequently required by node
+//     with the .es6, .es, .jsx, .mjs, and .js extensions
+//
+// See: https://babeljs.io/docs/en/babel-register
+//
+require('@babel/register')({
+  // Babelify everything except node_modules that are not our own stuff
+  ignore: [file => file.includes('node_modules') && !file.includes('@tanker')],
+  ...getBabelConfig({ target: 'node', coverage })
+});
