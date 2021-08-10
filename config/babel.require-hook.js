7b7eeb90797e34491d14d7013157472284bb426d
julien mounier <julien.mounier@tanker.io>
2021-08-10 17:43:00 +0200

    fix(mocha): rely on only one loader at a time

--- config/babel.require-hook.js
+++ config/babel.require-hook.js
@@ -19,10 +19,12 @@ babelConfig.plugins.push([
 // Babelify everything except node_modules that are not our own stuff
 babelConfig.ignore = [file => file.includes('node_modules') && !file.includes('@tanker')];
 
+// Automatically compile on the fly all the files subsequently required
+// by node with the .es6, .es, .jsx, .mjs, .js and .ts extensions
+babelConfig.extensions = ['.cjs', '.es', '.es6', '.js', '.jsx', '.mjs', '.ts', '.tsx'];
+
 // Install the "require hook" which will:
 //   - bind itself to node's require
-//   - automatically compile on the fly all the files subsequently required by node
-//     with the .es6, .es, .jsx, .mjs, and .js extensions
 //
 // See: https://babeljs.io/docs/en/babel-register
 //
