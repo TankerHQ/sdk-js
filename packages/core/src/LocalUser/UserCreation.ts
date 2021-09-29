c86c4c208c8a1b40cc011b240ac7fa4c56048a2e
julien mounier <julien.mounier@tanker.io>
2021-09-29 12:00:25 +0200

    fix(core): fix minor variable type declaration

--- packages/core/src/LocalUser/UserCreation.ts
+++ packages/core/src/LocalUser/UserCreation.ts
@@ -141,7 +141,7 @@ const rotateUserKeys = (devices: Array<Device>, currentUserKey: tcrypto.SodiumKe
 };
 
 export const makeDeviceRevocation = (devices: Array<Device>, currentUserKeys: tcrypto.SodiumKeyPair, deviceId: Uint8Array) => {
-  const remainingDevices: Device[] = [];
+  const remainingDevices: Array<Device> = [];
   let deviceToRevokeFound = false;
   let deviceAlreadyRevoked = false;
 
