675fa0ce09532d46b83099b95657dc9310933afc
Tristan Foch <tristan.floch@tanker.io>
2021-10-05 15:26:24 +0200

    feat(v6): made encryptionV6.decrypt callable by core

--- packages/crypto/src/index.ts
+++ packages/crypto/src/index.ts
@@ -12,8 +12,9 @@ import * as encryptionV2 from './EncryptionFormats/v2';
 import * as encryptionV3 from './EncryptionFormats/v3';
 import * as encryptionV4 from './EncryptionFormats/v4';
 import * as encryptionV5 from './EncryptionFormats/v5';
+import * as encryptionV6 from './EncryptionFormats/v6';
 
 export type { EncryptionFormatReporter } from './EncryptionFormats/types';
-export { aead, generichash, number, random, randomBase64Token, ready, tcrypto, utils, encryptionV1, encryptionV2, encryptionV3, encryptionV4, encryptionV5 };
+export { aead, generichash, number, random, randomBase64Token, ready, tcrypto, utils, encryptionV1, encryptionV2, encryptionV3, encryptionV4, encryptionV5, encryptionV6 };
 
 export type { b64string, safeb64string, Key };
