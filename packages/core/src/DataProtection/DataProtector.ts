d448d55a1118559ad004ce9d881d8454611d8b26
Tristan Foch <tristan.floch@tanker.io>
2021-10-29 16:52:30 +0200

    style: formatted dataprotector.ts and Tanker.spec.ts

--- packages/core/src/DataProtection/DataProtector.ts
+++ packages/core/src/DataProtection/DataProtector.ts
@@ -128,8 +128,8 @@ export class DataProtector {
       const trustchainIdB64 = utils.toBase64(this._localUser.trustchainId);
 
       if (!identities.some(identity => identity.target === 'user'
-                                    && identity.value === selfUserIdB64
-                                    && identity.trustchain_id === trustchainIdB64)) {
+        && identity.value === selfUserIdB64
+        && identity.trustchain_id === trustchainIdB64)) {
         return identities.concat([{ trustchain_id: trustchainIdB64, target: 'user', value: selfUserIdB64 }]);
       }
     }
