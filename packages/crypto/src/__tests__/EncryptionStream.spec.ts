b42d9017c142030f761ad8003f341b56d761d3ad
Philippe Daouadi <philippe.daouadi@doctolib.com>
2022-03-22 15:43:20 +0100

    tests(EncryptionStream): add tests for v8

--- packages/crypto/src/__tests__/EncryptionStream.spec.ts
+++ packages/crypto/src/__tests__/EncryptionStream.spec.ts
@@ -3,20 +3,32 @@ import { InvalidArgument } from '@tanker/errors';
 import { expect, BufferingObserver, makeTimeoutPromise } from '@tanker/test-utils';
 import { PromiseWrapper } from '@tanker/types';
 
+import { Padding, removePadding } from '../padding';
 import * as tcrypto from '../tcrypto';
 import { ready as cryptoReady } from '../ready';
 import * as aead from '../aead';
 import { random } from '../random';
 import * as utils from '../utils';
 import * as encryptionV4 from '../EncryptionFormats/v4';
+import * as encryptionV8 from '../EncryptionFormats/v8';
 import { EncryptionStreamV4 } from '../EncryptionFormats/EncryptionStreamV4';
+import { EncryptionStreamV8 } from '../EncryptionFormats/EncryptionStreamV8';
 
-describe('EncryptionStreamV4', () => {
+type EncryptionStream = EncryptionStreamV4 | EncryptionStreamV8;
+
+type TestParameters = {
+  makeEncryptionStream: (resourceId: Uint8Array, key: Uint8Array, chunkSize?: number) => EncryptionStream;
+  // this is encryptionV4 | encryptionV8, but they are not types, they're namespaces
+  encryption: any;
+  transformMsg: (message: Uint8Array) => Uint8Array,
+};
+
+const generateEncryptionStreamTests = ({ makeEncryptionStream, encryption, transformMsg }: TestParameters) => {
   let buffer: Array<Uint8Array>;
   let key: Uint8Array;
   let resourceId: Uint8Array;
 
-  const watchStream = (stream: EncryptionStreamV4) => {
+  const watchStream = (stream: EncryptionStream) => {
     const sync = new PromiseWrapper<void>();
     stream.on('data', (data: Uint8Array) => buffer.push(data));
     stream.on('error', (err: Error) => sync.reject(err));
@@ -36,7 +48,7 @@ describe('EncryptionStreamV4', () => {
   });
 
   it('throws InvalidArgument when writing anything else than Uint8Array', async () => {
-    const stream = new EncryptionStreamV4(resourceId, key);
+    const stream = makeEncryptionStream(resourceId, key);
     const sync = watchStream(stream);
 
     stream.write('fail');
@@ -46,7 +58,7 @@ describe('EncryptionStreamV4', () => {
   });
 
   it('can give its associated resourceId', async () => {
-    const stream = new EncryptionStreamV4(resourceId, key);
+    const stream = makeEncryptionStream(resourceId, key);
     const sync = watchStream(stream);
 
     expect(stream.resourceId).to.be.equal(utils.toBase64(resourceId));
@@ -56,58 +68,32 @@ describe('EncryptionStreamV4', () => {
   });
 
   it('outputs a resource from which you can read the header', async () => {
-    const stream = new EncryptionStreamV4(resourceId, key);
+    const stream = makeEncryptionStream(resourceId, key);
     const sync = watchStream(stream);
     stream.end();
     await sync.promise;
 
-    const data = encryptionV4.unserialize(buffer[0]!);
+    const data = encryption.unserialize(buffer[0]!);
 
     expect(data.resourceId).to.deep.equal(resourceId);
     expect(typeof data.encryptedChunkSize).to.equal('number');
   });
 
   it('outputs a resource from which you can directly get the resource id', async () => {
-    const stream = new EncryptionStreamV4(resourceId, key);
+    const stream = makeEncryptionStream(resourceId, key);
     const sync = watchStream(stream);
     stream.end();
     await sync.promise;
 
-    expect(encryptionV4.extractResourceId(buffer[0]!)).to.deep.equal(resourceId);
-  });
-
-  it('derives its iv and push header before encryption', async () => {
-    const msg = utils.fromString('message');
-    const stream = new EncryptionStreamV4(resourceId, key);
-    const sync = watchStream(stream);
-
-    stream.write(msg);
-
-    expect(sync.settled).to.be.false;
-
-    stream.end();
-
-    await expect(sync.promise).to.be.fulfilled;
-
-    expect(buffer.length).to.be.equal(1);
-    const data = encryptionV4.unserialize(buffer[0]!);
-
-    expect(data.resourceId).to.deep.equal(resourceId);
-
-    const eMsg = data.encryptedData;
-    const ivSeed = data.ivSeed;
-    const iv = tcrypto.deriveIV(ivSeed, 0);
-
-    expect(() => aead.decryptAEAD(key, ivSeed, eMsg)).to.throw();
-    expect(aead.decryptAEAD(key, iv, eMsg)).to.deep.equal(msg);
+    expect(encryption.extractResourceId(buffer[0]!)).to.deep.equal(resourceId);
   });
 
   it('encrypts chunks of fixed size', async () => {
     const msg = utils.fromString('message');
 
-    const encryptedChunkSize = msg.length + encryptionV4.overhead;
+    const encryptedChunkSize = msg.length + encryption.overhead;
 
-    const stream = new EncryptionStreamV4(resourceId, key, encryptedChunkSize);
+    const stream = makeEncryptionStream(resourceId, key, encryptedChunkSize);
     const sync = watchStream(stream);
 
     // push msg twice
@@ -122,18 +108,18 @@ describe('EncryptionStreamV4', () => {
     expect(buffer.length).to.equal(3);
 
     buffer.forEach((_, index) => {
-      const clearData = encryptionV4.decryptChunk(key, index, encryptionV4.unserialize(buffer[index]!));
+      const clearData = encryption.decryptChunk(key, index, encryption.unserialize(buffer[index]!));
       const expectedMsg = index === 2 ? new Uint8Array(0) : msg;
-      expect(clearData).to.deep.equal(expectedMsg);
+      expect(transformMsg(clearData)).to.deep.equal(expectedMsg);
     });
   });
 
   it('encrypts chunks of fixed size except last one', async () => {
     const msg = utils.fromString('message');
 
-    const encryptedChunkSize = msg.length + encryptionV4.overhead;
+    const encryptedChunkSize = msg.length + encryption.overhead;
 
-    const stream = new EncryptionStreamV4(resourceId, key, encryptedChunkSize);
+    const stream = makeEncryptionStream(resourceId, key, encryptedChunkSize);
     const sync = watchStream(stream);
 
     // push msg twice + 1 more byte
@@ -149,9 +135,9 @@ describe('EncryptionStreamV4', () => {
     expect(buffer.length).to.equal(3);
 
     buffer.forEach((_, index) => {
-      const clearData = encryptionV4.decryptChunk(key, index, encryptionV4.unserialize(buffer[index]!));
+      const clearData = encryption.decryptChunk(key, index, encryption.unserialize(buffer[index]!));
       const expectedMsg = index === 2 ? msg.subarray(1) : msg;
-      expect(clearData).to.deep.equal(expectedMsg);
+      expect(transformMsg(clearData)).to.deep.equal(expectedMsg);
     });
   });
   const coef = 3;
@@ -161,14 +147,14 @@ describe('EncryptionStreamV4', () => {
         const chunk = new Uint8Array(chunkSize);
         const inputSize = 10 * chunkSize;
         const bufferCounter = new BufferingObserver();
-        const encryptionStream = new EncryptionStreamV4(resourceId, key, chunkSize + encryptionV4.overhead);
+        const encryptionStream = makeEncryptionStream(resourceId, key, chunkSize + encryption.overhead);
         const timeout = makeTimeoutPromise(50);
         const slowWritable = new Writable({
           highWaterMark: 1,
           objectMode: true,
           write: async (data, _, done) => {
             await timeout.promise;
-            bufferCounter.incrementOutputAndSnapshot(data.length - encryptionV4.overhead);
+            bufferCounter.incrementOutputAndSnapshot(data.length - encryption.overhead);
             done();
           },
         });
@@ -201,5 +187,59 @@ describe('EncryptionStreamV4', () => {
         });
       });
     });
+  });
+};
+
+describe('EncryptionStreamV4', () => {
+  generateEncryptionStreamTests({
+    makeEncryptionStream: (resourceId: Uint8Array, key: Uint8Array, chunkSize?: number) => new EncryptionStreamV4(resourceId, key, chunkSize),
+    encryption: encryptionV4,
+    transformMsg: m => m,
+  });
+
+  it('derives its iv and push header before encryption', async () => {
+    const buffer: Array<Uint8Array> = [];
+    const key = utils.fromString('12345678123456781234567812345678');
+    const resourceId = random(tcrypto.MAC_SIZE);
+
+    const watchStream = (stream: EncryptionStream) => {
+      const sync = new PromiseWrapper<void>();
+      stream.on('data', (data: Uint8Array) => buffer.push(data));
+      stream.on('error', (err: Error) => sync.reject(err));
+      stream.on('end', () => sync.resolve());
+      return sync;
+    };
+
+    const msg = utils.fromString('message');
+    const stream = new EncryptionStreamV4(resourceId, key);
+    const sync = watchStream(stream);
+
+    stream.write(msg);
+
+    expect(sync.settled).to.be.false;
+
+    stream.end();
+
+    await expect(sync.promise).to.be.fulfilled;
+
+    expect(buffer.length).to.be.equal(1);
+    const data = encryptionV4.unserialize(buffer[0]!);
+
+    expect(data.resourceId).to.deep.equal(resourceId);
+
+    const eMsg = data.encryptedData;
+    const ivSeed = data.ivSeed;
+    const iv = tcrypto.deriveIV(ivSeed, 0);
+
+    expect(() => aead.decryptAEAD(key, ivSeed, eMsg)).to.throw();
+    expect(aead.decryptAEAD(key, iv, eMsg)).to.deep.equal(msg);
+  });
+});
+
+describe('EncryptionStreamV8', () => {
+  generateEncryptionStreamTests({
+    makeEncryptionStream: (resourceId: Uint8Array, key: Uint8Array, chunkSize?: number) => new EncryptionStreamV8(resourceId, key, Padding.OFF, chunkSize),
+    encryption: encryptionV8,
+    transformMsg: removePadding,
   });
 });
