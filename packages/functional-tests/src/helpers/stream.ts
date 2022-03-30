6b27a1e740b356dc755d1d75345e6cbfe7bde27a
Philippe Daouadi <philippe.daouadi@doctolib.com>
2022-03-30 15:12:53 +0200

    tests(functional): extract stream helper

--- packages/functional-tests/src/helpers/stream.ts
+++ packages/functional-tests/src/helpers/stream.ts
@@ -1,3 +1,4 @@
+import type { DecryptionStream, EncryptionStream } from '@tanker/core';
 import type { Readable, IWritable } from '@tanker/stream-base';
 
 // copy paste of the same function from core/src/CloudStorage/Manager.js
@@ -6,4 +7,11 @@ export const pipeStreams = async <T>(
 ) => new Promise<T>((resolve, reject) => {
   streams.forEach(stream => stream.on('error', reject));
   streams.reduce((leftStream, rightStream) => (leftStream as Readable).pipe(rightStream as IWritable)).on(resolveEvent, resolve);
+});
+
+export const watchStream = (stream: EncryptionStream | DecryptionStream) => new Promise<Array<Uint8Array>>((resolve, reject) => {
+  const result: Array<Uint8Array> = [];
+  stream.on('data', (data: Uint8Array) => result.push(data));
+  stream.on('end', () => resolve(result));
+  stream.on('error', reject);
 });
