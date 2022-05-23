93720647a6f822839833c2f8ae79617d831235c6
julien mounier <julien.mounier@doctolib.com>
2022-05-23 17:07:05 +0200

    fix(dumb-reducer): remove unused type

--- src/typescript/dumb-reducer/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'dumb-reducer' {
-  export type Reducer = (state: Record<string, any>, payload: Record<string, any>) => Record<string, any>;
-  export type SubReducers = Record<string, Reducer>;
-  export function makeDumbReducer(prefix: string, initialState: Record<string, any> = {}, subReducers: SubReducers = {}): Reducer;
-  export function prefixActions(prefix: string, actions: Record<string, any>): Record<string, any>; 
-}
