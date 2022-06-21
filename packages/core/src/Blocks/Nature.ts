30257ab2864ddacf526016e53db85a1a8a076280
Francisco Micaelli <francisco.micaelli@tanker.io>
2022-06-21 10:13:23 +0200

    feat: remove revocation nature and fix natureExists function

--- packages/core/src/Blocks/Nature.ts
+++ packages/core/src/Blocks/Nature.ts
@@ -4,12 +4,12 @@ export const NATURE = Object.freeze({
   trustchain_creation: 1,
   device_creation_v1: 2,
   key_publish_to_device: 3,
-  device_revocation_v1: 4,
+  // device_revocation_v1: 4,
   // user_reset: 5,
   device_creation_v2: 6,
   device_creation_v3: 7,
   key_publish_to_user: 8,
-  device_revocation_v2: 9,
+  // device_revocation_v2: 9,
   user_group_creation_v1: 10,
   key_publish_to_user_group: 11,
   user_group_addition_v1: 12,
@@ -29,13 +29,13 @@ const NATURE_INT = Object.values(NATURE);
 export type Nature = typeof NATURE[keyof typeof NATURE];
 
 export function natureExists(nature: number): nature is Nature {
-  return nature in NATURE_INT;
+  return NATURE_INT.includes(nature as Nature);
 }
 
 export const NATURE_KIND = Object.freeze({
   trustchain_creation: 0,
   device_creation: 1,
-  device_revocation: 2,
+  // device_revocation: 2,
   key_publish_to_device: 3,
   key_publish_to_user: 4,
   user_group_creation: 5,
@@ -56,7 +56,6 @@ export function preferredNature(kind: NatureKind): Nature {
     case NATURE_KIND.key_publish_to_user: return NATURE.key_publish_to_user;
     case NATURE_KIND.key_publish_to_user_group: return NATURE.key_publish_to_user_group;
     case NATURE_KIND.key_publish_to_provisional_user: return NATURE.key_publish_to_provisional_user;
-    case NATURE_KIND.device_revocation: return NATURE.device_revocation_v2;
     case NATURE_KIND.device_creation: return NATURE.device_creation_v3;
     case NATURE_KIND.user_group_creation: return NATURE.user_group_creation_v3;
     case NATURE_KIND.user_group_addition: return NATURE.user_group_addition_v3;
@@ -77,8 +76,6 @@ export function natureKind(val: Nature): NatureKind {
     case NATURE.key_publish_to_user: return NATURE_KIND.key_publish_to_user;
     case NATURE.key_publish_to_user_group: return NATURE_KIND.key_publish_to_user_group;
     case NATURE.key_publish_to_provisional_user: return NATURE_KIND.key_publish_to_provisional_user;
-    case NATURE.device_revocation_v1: return NATURE_KIND.device_revocation;
-    case NATURE.device_revocation_v2: return NATURE_KIND.device_revocation;
     case NATURE.user_group_creation_v1: return NATURE_KIND.user_group_creation;
     case NATURE.user_group_creation_v2: return NATURE_KIND.user_group_creation;
     case NATURE.user_group_creation_v3: return NATURE_KIND.user_group_creation;
