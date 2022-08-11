c4bb7a54238552895f0b4858cab3245530ec6a98
maximerety <maxime.rety@gmail.com>
2022-08-11 10:58:16 +0200

    chore(ci): update GA+Browserstack test configs (see details)
    * Update macOS/iOS/Safari versions
    * Use latest macOS to test latest Chrome/Firefox
    * Add tests on Windows 11 in GA+Browsertack tests (deactivated for now)
    * Add tests on Android 12 + latest Samsung browser
    * Add tests on latest Opera
    * Add tests on oldest browsers "not blocked" by Doctolib
        * Source of outdated/blocked versions in the monolith:
            * At the time of this commit: https://github.com/doctolib/doctolib/blob/f381579/app/controllers/concerns/outdated_browsers.rb
            * Latest: https://github.com/doctolib/doctolib/blob/master/app/controllers/concerns/outdated_browsers.rb
        * Don't use Windows 7 which isn't supported anymore
        * Next EOL: Windows 8.1 on 10 Jan 2023

--- config/karma/launchers.js
+++ config/karma/launchers.js
@@ -8,24 +8,43 @@ const customLaunchers = {
 
   // All sorts of BrowserStack browsers, you can create your using:
   // https://www.browserstack.com/automate/capabilities
+  ChromeWindows11: {
+    base: 'BrowserStack',
+    browser: 'Chrome',
+    os: 'Windows',
+    os_version: '11',
+  },
   ChromeWindows10: {
     base: 'BrowserStack',
     browser: 'Chrome',
     os: 'Windows',
     os_version: '10',
   },
-  Chrome70Windows7: {
+  Chrome70Windows8: {
     base: 'BrowserStack',
     browser: 'Chrome',
-    browser_version: '70.0',
+    browser_version: '70.0', // Oldest version not blocked for Doctolib users (see: https://github.com/doctolib/doctolib/blob/f381579/app/controllers/concerns/outdated_browsers.rb)
     os: 'Windows',
-    os_version: '7',
+    os_version: '8.1', // Oldest Windows version still supported - https://endoflife.date/windows
   },
   ChomeOSX: {
     base: 'BrowserStack',
     browser: 'Chrome',
     os: 'OS X',
-    os_version: 'Mojave',
+    os_version: 'Monterey',
+  },
+  Chrome70OSX: {
+    base: 'BrowserStack',
+    browser: 'Chrome',
+    browser_version: '70.0', // Oldest version not blocked for Doctolib users (see: https://github.com/doctolib/doctolib/blob/f381579/app/controllers/concerns/outdated_browsers.rb)
+    os: 'OS X',
+    os_version: 'Catalina',
+  },
+  EdgeWindows11: {
+    base: 'BrowserStack',
+    browser: 'Edge',
+    os: 'Windows',
+    os_version: '11',
   },
   EdgeWindows10: {
     base: 'BrowserStack',
@@ -33,12 +52,18 @@ const customLaunchers = {
     os: 'Windows',
     os_version: '10',
   },
-  Edge18Windows10: {
+  Edge88Windows8: {
     base: 'BrowserStack',
     browser: 'Edge',
-    browserVersion: '18.0',
+    browserVersion: '88.0', // Oldest version not blocked for Doctolib users (see: https://github.com/doctolib/doctolib/blob/f381579/app/controllers/concerns/outdated_browsers.rb)
     os: 'Windows',
-    os_version: '10',
+    os_version: '8.1', // Oldest Windows version still supported - https://endoflife.date/windows
+  },
+  FirefoxWindows11: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    os: 'Windows',
+    os_version: '11',
   },
   FirefoxWindows10: {
     base: 'BrowserStack',
@@ -46,63 +71,109 @@ const customLaunchers = {
     os: 'Windows',
     os_version: '10',
   },
-  Firefox64Windows8: {
+  Firefox78Windows8: {
     base: 'BrowserStack',
     browser: 'Firefox',
-    browser_version: '64.0',
+    browser_version: '78.0', // Oldest version not blocked for Doctolib users (see: https://github.com/doctolib/doctolib/blob/f381579/app/controllers/concerns/outdated_browsers.rb)
     os: 'Windows',
-    os_version: '8.1',
+    os_version: '8.1', // Oldest Windows version still supported - https://endoflife.date/windows
   },
   FirefoxOSX: {
     base: 'BrowserStack',
     browser: 'Firefox',
     os: 'OS X',
-    os_version: 'Mojave',
+    os_version: 'Monterey',
   },
-  Safari12Mojave: {
+  Firefox78OSX: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    browser_version: '78.0', // Oldest version not blocked for Doctolib users (see: https://github.com/doctolib/doctolib/blob/f381579/app/controllers/concerns/outdated_browsers.rb)
+    os: 'OS X',
+    os_version: 'Catalina', // Oldest macOS still supported - https://endoflife.date/macos
+  },
+  OperaOSX: {
+    base: 'BrowserStack',
+    browser: 'Opera',
+    os: 'OS X',
+    os_version: 'Monterey',
+  },
+  Safari15Monterey: {
     base: 'BrowserStack',
     browser: 'Safari',
-    browser_version: '12.0',
+    browser_version: '15.3',
     os: 'OS X',
-    os_version: 'Mojave',
+    os_version: 'Monterey',
   },
-  Safari10Sierra: {
+  Safari14BigSur: {
     base: 'BrowserStack',
     browser: 'Safari',
-    browser_version: '10.0',
+    browser_version: '14.1',
     os: 'OS X',
-    os_version: 'Sierra',
+    os_version: 'Big Sur',
   },
-  iOS12: {
+  Safari13Catalina: {
+    base: 'BrowserStack',
+    browser: 'Safari',
+    browser_version: '13.1', // Oldest version not blocked for Doctolib patients (see: https://github.com/doctolib/doctolib/blob/f381579/app/controllers/concerns/outdated_browsers.rb)
+    os: 'OS X',
+    os_version: 'Catalina',
+  },
+  iOS15: {
+    base: 'BrowserStack',
+    device: 'iPhone 13',
+    real_mobile: 'true',
+    os: 'ios',
+    os_version: '15',
+  },
+  iOS14: {
+    base: 'BrowserStack',
+    device: 'iPhone 12',
+    real_mobile: 'true',
+    os: 'ios',
+    os_version: '14',
+  },
+  iOS13: {
     base: 'BrowserStack',
     device: 'iPhone XS',
     real_mobile: 'true',
     os: 'ios',
-    os_version: '12.1',
+    os_version: '13', // Oldest version not blocked for Doctolib patients (see: https://github.com/doctolib/doctolib/blob/f381579/app/controllers/concerns/outdated_browsers.rb)
   },
-  iOS10: {
+  Android12Samsung: {
     base: 'BrowserStack',
-    device: 'iPhone 7',
+    browser: 'samsung', // version 17.0 (as of 2022-08-18) - https://en.wikipedia.org/wiki/Samsung_Internet
+    device: 'Samsung Galaxy S22',
     real_mobile: 'true',
-    os: 'ios',
-    os_version: '10.0',
+    os: 'Android',
+    os_version: '12.0',
   },
-  Android10: {
+  Android12Chrome: {
     base: 'BrowserStack',
+    browser: 'chrome',
+    device: 'Samsung Galaxy S22',
+    real_mobile: 'true',
+    os: 'Android',
+    os_version: '12.0',
+  },
+  Android10Chrome: {
+    base: 'BrowserStack',
+    browser: 'chrome',
     device: 'Samsung Galaxy S20',
     real_mobile: 'true',
     os: 'Android',
     os_version: '10.0',
   },
-  Android8: {
+  Android8Chrome: {
     base: 'BrowserStack',
+    browser: 'chrome',
     device: 'Samsung Galaxy S9',
     real_mobile: 'true',
     os: 'Android',
     os_version: '8.0',
   },
-  Android6: {
+  Android6Chrome: {
     base: 'BrowserStack',
+    browser: 'chrome',
     device: 'Samsung Galaxy S7',
     real_mobile: 'true',
     os: 'Android',
