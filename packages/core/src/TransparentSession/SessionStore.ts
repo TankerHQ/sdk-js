3dd5b417480dbf7ebee55079a3fa25a3e54f0135
julien mounier <julien.mounier@doctolib.com>
2022-10-14 17:10:53 +0200

    feat(transparent-session): add session storage

---
+++ packages/core/src/TransparentSession/SessionStore.ts
@@ -0,0 +1,97 @@
+import type { b64string, Key } from '@tanker/crypto';
+import { utils, EncryptionV1 } from '@tanker/crypto';
+import type { DataStore } from '@tanker/datastore-base';
+import { errors as dbErrors } from '@tanker/datastore-base';
+import { InternalError } from '@tanker/errors';
+
+const TABLE = 'session_keys';
+
+export type SessionResult = {
+  id: b64string;
+  key: Key;
+};
+
+export const sessionTTL = 12 * 3600;
+
+export class TransparentSessionStore {
+  declare _ds: DataStore;
+  declare _userSecret: Uint8Array;
+
+  static schemas = [
+    // this store didn't exist before schema version 14
+    { version: 1, tables: [] },
+    { version: 2, tables: [] },
+    { version: 3, tables: [] },
+    { version: 4, tables: [] },
+    { version: 5, tables: [] },
+    { version: 6, tables: [] },
+    { version: 7, tables: [] },
+    { version: 8, tables: [] },
+    { version: 9, tables: [] },
+    { version: 10, tables: [] },
+    { version: 11, tables: [] },
+    { version: 12, tables: [] },
+    { version: 13, tables: [] },
+    { version: 14, tables: [{ name: TABLE }] },
+  ];
+
+  constructor(ds: DataStore, userSecret: Uint8Array) {
+    if (!userSecret)
+      throw new InternalError('Invalid user secret');
+
+    // _ properties won't be enumerable, nor reconfigurable
+    Object.defineProperty(this, '_ds', { value: ds, writable: true });
+    Object.defineProperty(this, '_userSecret', { value: userSecret }); // + not writable
+  }
+
+  now(): number {
+    return Date.now() / 1000;
+  }
+
+  async saveSessionKey(recipientsHash: Uint8Array, sessionId: Uint8Array, key: Key): Promise<void> {
+    // prevent db corruption by using the recipientsHash, sessionId and createdAt as additional data
+    const createdAt = this.now();
+    const associatedData = utils.concatArrays(recipientsHash, sessionId, utils.fromString(createdAt.toString()));
+    const encryptedKey = EncryptionV1.serialize(EncryptionV1.encrypt(this._userSecret, key, associatedData));
+    const b64RecipientsHash = utils.toBase64(recipientsHash);
+
+    await this._ds.put(TABLE, { _id: b64RecipientsHash, b64sessionId: utils.toBase64(sessionId), b64EncryptedKey: utils.toBase64(encryptedKey), createdAt });
+  }
+
+  async findSessionKey(recipientsHash: Uint8Array): Promise<SessionResult | null> {
+    try {
+      const b64RecipientsHash = utils.toBase64(recipientsHash);
+      const result = await this._ds.get(TABLE, b64RecipientsHash);
+      const createdAt = result['createdAt'];
+      const currentTime = this.now();
+      if (currentTime < createdAt || currentTime > createdAt + sessionTTL) {
+        return null;
+      }
+
+      const id = result['b64sessionId'];
+      const associatedData = utils.concatArrays(recipientsHash, utils.fromBase64(id), utils.fromString(createdAt.toString()));
+      const encryptedKey = utils.fromBase64(result['b64EncryptedKey']!);
+      return {
+        id,
+        key: await EncryptionV1.decrypt(() => this._userSecret, EncryptionV1.unserialize(encryptedKey), associatedData),
+      };
+    } catch (e) {
+      if (e instanceof dbErrors.RecordNotFound) {
+        return null;
+      }
+      throw e;
+    }
+  }
+
+  async close(): Promise<void> {
+    // Erase traces of critical data first
+    utils.memzero(this._userSecret);
+
+    // @ts-expect-error
+    this._ds = null;
+  }
+
+  static async open(ds: DataStore, userSecret: Uint8Array): Promise<TransparentSessionStore> {
+    return new TransparentSessionStore(ds, userSecret);
+  }
+}
---
+++ packages/core/src/TransparentSession/__tests__/TransparentSession.spec.ts
@@ -0,0 +1,142 @@
+import { random, ready as cryptoReady, tcrypto, utils } from '@tanker/crypto';
+import { createUserSecretBinary } from '@tanker/identity';
+import { expect, sinon } from '@tanker/test-utils';
+
+import dataStoreConfig, { makePrefix, openDataStore } from '../../__tests__/TestDataStore';
+
+import { TransparentSessionStore } from '../SessionStore';
+import { computeRecipientHash } from '../Manager';
+
+const makeSession = () => ({
+  id: random(tcrypto.SESSION_ID_SIZE),
+  key: random(tcrypto.SYMMETRIC_KEY_SIZE),
+});
+
+describe('TransparentSessionStore', () => {
+  let stub: sinon.SinonStub | null = null;
+  let sessionStore: TransparentSessionStore;
+  let hash: Uint8Array;
+
+  before(() => cryptoReady);
+
+  afterEach(() => {
+    if (stub !== null)
+      stub.restore();
+    stub = null;
+  });
+
+  beforeEach(async () => {
+    const dbName = `sessionStore-test-${makePrefix()}`;
+    const userSecret = createUserSecretBinary('trustchainid', 'Merkle–Damgård');
+    const sessionStoreConfig = { dbName, ...dataStoreConfig, schemas: TransparentSessionStore.schemas };
+    const datastore = await openDataStore(sessionStoreConfig);
+    sessionStore = await TransparentSessionStore.open(datastore, userSecret);
+    hash = random(32);
+  });
+
+  it('saves and finds session keys', async () => {
+    const session = makeSession();
+
+    await sessionStore.saveSessionKey(hash, session.id, session.key);
+
+    expect(await sessionStore.findSessionKey(hash)).to.deep.equal({
+      id: utils.toBase64(session.id),
+      key: session.key,
+    });
+  });
+
+  it('overrides session', async () => {
+    let session = makeSession();
+
+    await sessionStore.saveSessionKey(hash, session.id, session.key);
+
+    session = makeSession();
+    await sessionStore.saveSessionKey(hash, session.id, session.key);
+
+    expect(await sessionStore.findSessionKey(hash)).to.deep.equal({
+      id: utils.toBase64(session.id),
+      key: session.key,
+    });
+  });
+
+  it('does not find outdated session keys', async () => {
+    const nowStub = sinon.stub(sessionStore, 'now');
+    stub = nowStub;
+    nowStub.returns(new Date('2022-01-01T00:00').getTime());
+    const session = makeSession();
+
+    await sessionStore.saveSessionKey(hash, session.id, session.key);
+    // one day later
+    nowStub.returns(new Date('2022-01-02T00:00').getTime());
+
+    expect(await sessionStore.findSessionKey(hash)).to.be.null;
+  });
+
+  it('does not find session keys form the future', async () => {
+    const nowStub = sinon.stub(sessionStore, 'now');
+    stub = nowStub;
+    nowStub.returns(new Date('2022-01-02T00:00').getTime());
+    const session = makeSession();
+
+    await sessionStore.saveSessionKey(hash, session.id, session.key);
+    // one day earlier
+    nowStub.returns(new Date('2022-01-01T00:00').getTime());
+
+    expect(await sessionStore.findSessionKey(hash)).to.be.null;
+  });
+});
+
+describe('computeRecipientHash', () => {
+  it('compute different hash for different lists', () => {
+    const ids = [utils.toBase64(random(32)), utils.toBase64(random(32)), utils.toBase64(random(32))];
+
+    for (let index = 0; index < ids.length; index++) {
+      expect(
+        computeRecipientHash({ shareWithUsers: ids.slice(index) }),
+      ).to.not.deep.equal(
+        computeRecipientHash({ shareWithGroups: ids.slice(index) }),
+      );
+    }
+
+    for (let index = 0; index < ids.length; index++) {
+      expect(
+        computeRecipientHash({ shareWithUsers: [ids[0]!] }),
+      ).to.not.deep.equal(
+        computeRecipientHash({ shareWithUsers: ids.slice(2) }),
+      );
+    }
+
+    expect(
+      computeRecipientHash({ shareWithUsers: ids.slice(0, 1) }),
+    ).to.not.deep.equal(
+      computeRecipientHash({ shareWithUsers: ids.slice(1) }),
+    );
+  });
+
+  it('ignores order inside arrays', () => {
+    const id1 = utils.toBase64(random(32));
+    const id2 = utils.toBase64(random(32));
+
+    const hash = computeRecipientHash({
+      shareWithGroups: [id1, id2],
+    });
+
+    expect(computeRecipientHash({
+      shareWithGroups: [id2, id1],
+    })).to.deep.equal(hash);
+  });
+
+  it('changes when sharing w/ and w/o self', () => {
+    const groupId = utils.toBase64(random(32));
+
+    const hash = computeRecipientHash({
+      shareWithGroups: [groupId],
+      shareWithSelf: true,
+    });
+
+    expect(computeRecipientHash({
+      shareWithGroups: [groupId],
+      shareWithSelf: false,
+    })).to.not.deep.equal(hash);
+  });
+});
