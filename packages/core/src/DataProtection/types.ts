e80492e40cc18968eea09a9ac5f2848e685453ee
julien mounier <julien.mounier@doctolib.com>
2022-11-02 23:08:43 +0100

    fix(upload): refactor stream format selection

--- packages/core/src/DataProtection/types.ts
+++ packages/core/src/DataProtection/types.ts
@@ -1,5 +1,5 @@
-import type { Key, EncryptionFormatDescription } from '@tanker/crypto';
-import { EncryptionV3, EncryptionV4, EncryptionV5, EncryptionV6, EncryptionV7, EncryptionV8, random, tcrypto, Padding } from '@tanker/crypto';
+import type { Key } from '@tanker/crypto';
+import { EncryptionV3, EncryptionV5, EncryptionV6, EncryptionV7, random, tcrypto, Padding } from '@tanker/crypto';
 
 export type Resource = {
   resourceId: Uint8Array;
@@ -15,15 +15,3 @@ export function makeResource(): Resource {
 export const getSimpleEncryption = (paddingStep?: number | Padding) => (paddingStep === Padding.OFF ? EncryptionV3 : EncryptionV6);
 
 export const getSimpleEncryptionWithFixedResourceId = (paddingStep?: number | Padding) => (paddingStep === Padding.OFF ? EncryptionV5 : EncryptionV7);
-
-export const getStreamEncryptionFormatDescription = (paddingStep?: number | Padding): EncryptionFormatDescription => {
-  if (paddingStep === Padding.OFF)
-    return ({
-      version: 4,
-      encryptedChunkSize: EncryptionV4.defaultMaxEncryptedChunkSize,
-    });
-  return ({
-    version: 8,
-    encryptedChunkSize: EncryptionV8.defaultMaxEncryptedChunkSize,
-  });
-};
