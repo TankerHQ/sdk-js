b37e2c12810b32203d65cf3ff508654d5d596eeb
julien mounier <julien.mounier@doctolib.com>
2024-07-17 13:24:13 +0200

    fix(tests): migrate fake-oidc endpoints to /issuers/fake-oidc

--- packages/functional-tests/src/verification.ts
+++ packages/functional-tests/src/verification.ts
@@ -910,7 +910,7 @@ export const generateVerificationTests = (args: TestArgs) => {
       describe('verification by oidc authorization code', () => {
         let provider: { id: string, display_name: string };
 
-        const setFakeOidcSubjectCookie = async (subject: string) => fetch(`${oidcSettings.fakeOidc.url}/issuer/subject`, {
+        const setFakeOidcSubjectCookie = async (subject: string) => fetch(`${oidcSettings.fakeOidc.url}/issuers/main/subject`, {
           method: 'POST',
           body: JSON.stringify({ subject }),
           credentials: 'include',
